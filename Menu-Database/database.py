a5d4349f4d75c74cbe9e478c1189c452baa54c2e
Katerina Sofroniou <katerina.sof@btinternet.com>
2023-01-25 16:32:31 +0000

    Moved certain elements of file for readability

--- Menu-Database/database.py
+++ Menu-Database/database.py
@@ -3,14 +3,17 @@ from config import DATABASE_URI
 from models import Base
 from insert import ins
 
+# Creating  a connection to the database
 engine = create_engine(DATABASE_URI)
 Base.metadata.create_all(engine)
+Session = engine.connect()
 
+# Drops all tables if they exist and creates them again
 def recreate_database():
     Base.metadata.drop_all(engine)
     Base.metadata.create_all(engine)
-    
-Session = engine.connect()
 
 recreate_database()
+
+# Executes the first insert statement
 Session.execute(ins)
