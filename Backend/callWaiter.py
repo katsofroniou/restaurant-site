1fd581be6683c58c1ea41b891bddd536d44c0e89
Katerina Sofroniou <katerina.sof@btinternet.com>
2023-01-26 21:00:58 +0000

    chore: removed unused import

---
+++ Backend/callWaiter.py
@@ -0,0 +1,13 @@
+from flask import Flask, request
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def button_pressed():
+    if request.method == 'POST':
+        if request.form.get('waiter_button') == 'waiter_button':
+            return "Waiter has been called"
+    return "waiting"
+ 
+if __name__ == '__main__':
+    app.run(debug = True)
