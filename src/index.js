29971751091e8baf2e6aa776e6eb8ae12144115b
Davit <zkac151@live.rhul.ac.uk>
2023-02-02 22:29:56 +0000

    feat: Added route path for login

--- src/index.js
+++ src/index.js
@@ -13,6 +13,7 @@ export default function App() {
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
           <Route path="menu" element={<Menu />} />
+          <Route path="login" element={<Login/>} />
           <Route path="*" element={<NotFound />} />
         </Route>
       </Routes>
