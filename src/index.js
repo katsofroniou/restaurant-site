4f27ca9a031985e22f670eaf92f64d026c846f32
zkac123 <Natalia.Widmann.2021@live.rhul.ac.uk>
2023-02-02 16:27:34 +0000

    feat: added routing

--- src/index.js
+++ src/index.js
@@ -6,14 +6,21 @@ import Menu from "./pages/Menu";
 import NotFound from "./pages/NotFound";
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-ReactDOM.render((
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>),
-  document.getElementById('root')
-);
+export default function App() {
+  return(
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="menu" element={<Menu />} />
+          <Route path="*" element={<NotFound />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
 
+ReactDOM.render(<App />, document.getElementById("root"));
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
