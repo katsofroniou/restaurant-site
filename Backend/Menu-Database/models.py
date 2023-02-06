73f246d10eee54ed35922a27aa6d946830db6a12
Katerina Sofroniou <katerina.sof@btinternet.com>
2023-02-06 23:28:58 +0000

    feat: added employees and addresses table

--- Backend/Menu-Database/models.py
+++ Backend/Menu-Database/models.py
@@ -1,28 +1,41 @@
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, Integer, String, Boolean, Float
+from sqlalchemy import Column, Integer, \
+    String, Boolean, Float, ForeignKey
 
 Base = declarative_base()
 
-class oaxaca(Base):
+class Dishes(Base):
     
     # Creates a new table and assigns new columns
     __tablename__ = 'dishes'
     id = Column(Integer, primary_key=True, autoincrement=True)
-    name = Column(String)
-    description = Column(String)
-    allergens = Column(String)
-    kcal = Column(Integer)
-    course = Column(String) # Starter, main, dessert, side, drink
-    price = Column(Float)
-    vegetarian = Column(Boolean)
-    vegan = Column(Boolean)
+    name = Column(String, nullable=False)
+    description = Column(String, nullable=False)
+    allergens = Column(String, nullable=True)
+    kcal = Column(Integer, nullable=False)
+    course = Column(String, nullable=False) # Starter, main, dessert, side, drink
+    price = Column(Float, nullable=False)
+    vegetarian = Column(Boolean, nullable=False)
+    vegan = Column(Boolean, nullable=False)
+
+class Address(Base):
+    __tablename__ = 'addresses'
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    line1 = Column(String, nullable=False)
+    line2 = Column(String, nullable=True)
+    building_number = Column(String, nullable=True)
+    city = Column(String, nullable=False)
+    state_or_province = Column(String, nullable=False)
+    country = Column(String, nullable=False)
+    postcode_zip = Column(String, nullable=False) 
+
+class Employees(Base):
+    __tablename__ = 'employees'
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    first_name = Column(String, nullable=False)
+    last_name = Column(String, nullable=False)
+    email = Column(String, nullable=False)
+    phone = Column(String, nullable=False)
+    address_id = Column(Integer, ForeignKey('addresses.id'), nullable=False)
+    currently_employed = Column(Boolean, nullable=False)
     
-    
-    def __repr__(self):
-        return "<MenuItems(id='{}', name='{}', description='{}', \
-            allergens='{}', kcal='{}', course='{}', price='{}'\
-            vegetarian='{}', vegan='{}')>"\
-                .format(self.id, self.name, self.description, \
-                    self.allergens, self.kcal, self.course, \
-                        self.price, self.vegetarian, self.vegan)
-                
