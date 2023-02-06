9140e0c0ddc19b069359c63f57df0fbe6b0f2941
zkac123 <Natalia.Widmann.2021@live.rhul.ac.uk>
2023-02-06 15:45:03 +0000

    fix: added div in Navbar function

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -4,22 +4,24 @@ import "./styling/Navbar.css";
 
 function Navbar () {
     return (
-        <nav class="navbar">
-            <div class="navbar_container">
-                <p id="navbar_logo">OAXACA</p>
-            </div>
-            <ul class="navbar_menu">
-                <li class="navbar_item">
-                    <Link to='/' class="navbar_links">Home</Link> 
-                </li>
-                <li class="navbar_item">
-                    <Link to='/Menu' class="navbar_links">Menu</Link>
-                </li>
-                <li class="navbar_btn">
+        <>
+            <nav class="navbar">
+                <div class="navbar_container">
+                    <p id="navbar_logo">OAXACA</p>
+                </div>
+                <ul class="navbar_menu">
+                    <li class="navbar_item">
+                        <Link to='/' class="navbar_links">Home</Link>
+                    </li>
+                    <li class="navbar_item">
+                        <Link to='/Menu' class="navbar_links">Menu</Link>
+                    </li>
+                    <li class="navbar_btn">
                         <Link to='/Login' class="navbar_button">Sign in</Link>
-                </li>
-            </ul>
-        </nav>
+                    </li>
+                </ul>
+            </nav>
+        </>
     );
 }
 
