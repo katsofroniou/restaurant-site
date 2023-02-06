bf7b814a92b621cd3b4bd3d1c1d0d21fda1f931d
zkac237 <zkac237@live.rhul.ac.uk>
2023-02-06 16:23:56 +0000

    feat: Filter the database by menu heading or a specific item in menu heading

---
+++ Backend/filter_by_heading.py
@@ -0,0 +1,25 @@
+from sqlalchemy.sql import text
+from sqlalchemy import create_engine
+from config import DATABASE_URI
+from models import Base
+from insert import addDishes
+from sqlalchemy.engine.url import URL
+from sqlalchemy.orm import sessionmaker
+from models import oaxaca
+from sqlalchemy import insert
+import json
+
+engine = create_engine(DATABASE_URI)
+Base.metadata.create_all(engine)
+Session = engine.connect()
+
+
+
+def addDishes(Session):
+    with open('Menu-Database/dishes.json', 'r') as f:
+        dishes = json.load(f)
+
+Session = sessionmaker(bind = engine)
+session = Session()
+result = session.query(oaxaca).filter(oaxaca.name)
+
