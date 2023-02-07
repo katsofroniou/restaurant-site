491beea8a8f7bcb86977a543d1a498178a53b0ef
Katerina Sofroniou <katerina.sof@btinternet.com>
2023-02-07 23:28:43 +0000

    fix: removed unused imports and code

--- Backend/Menu-Database/filter_by_heading.py
+++ Backend/Menu-Database/filter_by_heading.py
@@ -1,25 +1,5 @@
-from sqlalchemy.sql import text
-from sqlalchemy import create_engine
-from config import DATABASE_URI
-from models import Base
-from insert import addDishes
-from sqlalchemy.engine.url import URL
-from sqlalchemy.orm import sessionmaker
-from models import oaxaca
-from sqlalchemy import insert
-import json
+from models import Dishes
+from main import Session
 
-engine = create_engine(DATABASE_URI)
-Base.metadata.create_all(engine)
-Session = engine.connect()
-
-
-
-def addDishes(Session):
-    with open('Menu-Database/dishes.json', 'r') as f:
-        dishes = json.load(f)
-
-Session = sessionmaker(bind = engine)
-session = Session()
-result = session.query(oaxaca).filter(oaxaca.name)
+result = Session.query(Dishes).filter(Dishes.name)
 
