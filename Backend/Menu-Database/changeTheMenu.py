75872bbd5b72c2d135cadd6a0ab8265f60287f45
zkac189 <zkac189@live.rhul.ac.uk>
2023-02-07 00:43:29 +0000

    fix: got rid of unused lines and edited delete to search using primary key

--- Backend/Menu-Database/changeTheMenu.py
+++ Backend/Menu-Database/changeTheMenu.py
@@ -1,8 +1,7 @@
-from database import Session
 from models import oaxaca
-from sqlalchemy.sql import text
+from main import Session
 from sqlalchemy import delete
-from sqlalchemy import insert
+
 
 #add item
 def addItem(Session):
@@ -10,15 +9,15 @@ def addItem(Session):
     VALUES(475, 'Cheeseburger', 'Mature cheddar cheese, red onion rings, lettuce, tomato, ketchup', '{Milk, Eggs, Gluten}',
     '800', 'Main', 6.99, false, false)"""
 
-    stmt = (insert(oaxaca).values(id=1234, name='Cheeseburger', description='Mature cheddar cheese, red onion rings, lettuce, tomato, ketchup', allergens='{Milk, Eggs, Gluten', kcal=800, course='Main', price=6.99, vegetarian=False, vegan=False))
     return Session.execute(query)
 
 #delete item
 def deleteItem(Session):
-    query = delete().where(oaxaca.name == "Cheeseburger")
+    query = delete().where(oaxaca.id == "475")
     Session.execute(query)
 
-
 #alter item
-#addItem(Session)
+
+
 addItem(Session)
+deleteItem(Session)
