5d4957259c2a8fd2c8fca619697d233afbb11255
Davit <zkac151@live.rhul.ac.uk>
2023-02-07 19:26:18 +0000

    feat: Created Notify Waiters Button

--- src/pages/KitchenStaff.jsx
+++ src/pages/KitchenStaff.jsx
@@ -3,7 +3,10 @@ import React from "react";
 function KitchenStaff () {
     return (
         <>
+            <h1 class="header">Kitchen</h1>
+            <button class="kitchen_button" type="button">Notify Waiters</button>
 
+            
         </>
     );
 }
