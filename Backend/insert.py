f5fc0e92f5eda8077d3a64102ca7ae5ac3a7871b
Katerina Sofroniou <katerina.sof@btinternet.com>
2023-02-08 02:23:42 +0000

    feat: created function to add data to logins table
    fix: changed json file paths in functions

--- Backend/insert.py
+++ Backend/insert.py
@@ -1,15 +1,26 @@
 from sqlalchemy import insert
-from models import Dishes
+from models import Dishes, Logins
 import json
 
 def addDishes(Session):
-    with open('Backend/dishes.json', 'r') as f:
+    with open('Backend/json-files/dishes.json', 'r') as f:
         dishes = json.load(f)
 
     for dish in dishes:
         ins = insert(Dishes).values(name=dish["name"], \
         description=dish["description"], allergens=dish["allergens"], \
-            kcal=dish["calories"], course=dish["course"], price=dish["price"], \
-                vegetarian=dish["vegetarian"], vegan=dish["vegan"])
+        kcal=dish["calories"], course=dish["course"], price=dish["price"], \
+        vegetarian=dish["vegetarian"], vegan=dish["vegan"])
+        
+        Session.execute(ins)
+        
+def addLogin(Session):
+    with open('Backend/json-files/logins.json', 'r') as f:
+        users = json.load(f)
+        
+    for user in users:
+        ins = insert(Logins).values(email=user["email"], \
+            first_name=user["first_name"], last_name=user["last_name"],\
+            username=user["username"], password=user["password"])
         
         Session.execute(ins)
