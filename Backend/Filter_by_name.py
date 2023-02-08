5b86aae3df20f12a3dcad0caefc3444b7f3e622b
Katerina Sofroniou <katerina.sof@btinternet.com>
2023-02-08 22:55:23 +0000

    refactor: deleted outdated backend files

--- Backend/Filter_by_name.py
+++
@@ -1,4 +0,0 @@
-from main import Session
-from models import Dishes
-
-results = Session.query(Dishes).filter(Dishes.name.like("%Chicken%")).all()
--- Backend/filter_by_heading.py
+++
@@ -1,5 +0,0 @@
-from models import Dishes
-from main import Session
-
-result = Session.query(Dishes).filter(Dishes.name)
-
