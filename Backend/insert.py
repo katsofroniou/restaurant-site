89656bba5f4d18ff18e23724c18bdd1318b6b8e9
Katerina Sofroniou <katerina.sof@btinternet.com>
2023-02-08 02:10:28 +0000

    chore: removed Menu-Database folder
    it was holding more than just the menu database

--- Backend/insert.py
+++ Backend/insert.py
@@ -1,15 +1,15 @@
-from models import Dishes
 from sqlalchemy import insert
+from models import Dishes
 import json
 
 def addDishes(Session):
-    with open('Backend/Menu-Database/dishes.json', 'r') as f:
+    with open('Backend/dishes.json', 'r') as f:
         dishes = json.load(f)
 
     for dish in dishes:
-        ins = insert(dishes).values(name=dish["name"], \
+        ins = insert(Dishes).values(name=dish["name"], \
         description=dish["description"], allergens=dish["allergens"], \
-            kcal=dish["calories"], course=dish["course"], price=4.99, \
+            kcal=dish["calories"], course=dish["course"], price=dish["price"], \
                 vegetarian=dish["vegetarian"], vegan=dish["vegan"])
         
         Session.execute(ins)
