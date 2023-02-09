85de5ee464ba313728c93742f62193bffb780607
Katerina Sofroniou <katerina.sof@btinternet.com>
2023-02-09 21:04:34 +0000

    feat: created api view for dishes

--- oaxaca/menu/views.py
+++ oaxaca/menu/views.py
@@ -1,7 +1,70 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status, permissions
+from .models import Dish
+from .serializers import DishSerializer
 
-# Create your views here.
-def front(request):
+class DishApiView(APIView):
+    permission_classes = [permissions.IsAuthenticated]
     
-    context = { }
-    return render(request, "index.html", context)
+    def getCourse(self, request, *arg, **kwargs):
+        dishCourse = Dish.objects.filter(dish = request.Dish.course)
+        serializer = DishSerializer(dishCourse, many=True)
+        return Response(serializer.data, status.HTTP_200_OK)
+    
+    def getAllergens(self, request, *arg, **kwargs):
+        dishAllergens = Dish.objects.filter(dish = request.Dish.allergens)
+        serializer = DishSerializer(dishAllergens, many=True)
+        return Response(serializer.data, status.HTTP_200_OK)
+    
+    def getPrice(self, request, *arg, **kwargs):
+        dishPrice = Dish.objects.filter(dish = request.Dish.price)
+        serializer = DishSerializer(dishPrice, many=True)
+        return Response(serializer.data, status.HTTP_200_OK)
+    
+    def getName(self, request, *arg, **kwargs):
+        dishName = Dish.objects.filter(dish = request.Dish.name)
+        serializer = DishSerializer(dishName, many=True)
+        return Response(serializer.data, status.HTTP_200_OK)
+    
+    def getVegetarian(self, request, *arg, **kwargs):
+        dishVeg = Dish.objects.filter(dish = request.Dish.vegetarian)
+        serializer = DishSerializer(dishVeg, many=True)
+        return Response(serializer.data, status.HTTP_200_OK)
+    
+    def getVegan(self, request, *arg, **kwargs):
+        dishVeg = Dish.objects.filter(dish = request.Dish.vegan)
+        serializer = DishSerializer(dishVeg, many=True)
+        return Response(serializer.data, status.HTTP_200_OK)
+    
+    def getKcal(self, request, *arg, **kwargs):
+        dishKcal = Dish.objects.filter(dish = request.Dish.kcal)
+        serializer = DishSerializer(dishKcal, many=True)
+        return Response(serializer.data, status.HTTP_200_OK)
+    
+    def getAvailable(self, request, *arg, **kwargs):
+        dishAvailable = Dish.objects.filter(dish = request.Dish.available)
+        serializer = DishSerializer(dishAvailable, many=True)
+        return Response(serializer.data, status.HTTP_200_OK)##
+    
+    def post(self, request, *args, **kwargs):
+        data = {
+            'name': request.data.get('name'),
+            'description': request.data.get('description'),
+            'allergens': request.data.get('allergens'),
+            'kcal': request.data.get('kcal'),
+            'course': request.data.get('course'),
+            'price': request.data.get('price'),
+            'vegetarian': request.data.get('vegetarian'),
+            'vegan': request.data.get('vegan'),
+            'available': request.data.get('available')
+        }
+        
+        serializer = DishSerializer(data=data)
+        
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)        
