0186c1ecb7ecad6798a95579f9a7b3742534d7d1
Jubai-Khalil <jubai.khalil@gmail.com>
2023-02-09 22:43:34 +0000

    Feat: func to connect kitchen button to notify waiter to backend

--- src/pages/KitchenStaff.jsx
+++ src/pages/KitchenStaff.jsx
@@ -1,12 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import "../styling/KitchenStaff.css";
 
 function KitchenStaff () {
+    const [orderReady, setOrderReady] = useState(false);
+    const handleOrderReady = () => {
+        fetch('/order-ready/', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({notification_type: "order_ready"})
+        })
+        .then(response => response.json())
+        .then(data => {
+            setOrderReady(true);
+            console.log("Kitchen staff has notified waiter order is ready.");
+        })
+        .catch(error => {
+            console.error('Error:', error);
+        });
+    };
     return (
         <>
             <h1 class="header">Kitchen</h1>
-            <button class="kitchen_button" type="button">Notify Waiters</button>
-
+            <button className="menu_button" onClick={handleOrderReady} disabled={orderReady}>
+                {orderReady ? "Notify Waiter" : "Order Ready"}
+            </button>
             
         </>
     );
