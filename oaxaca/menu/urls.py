bf10e7a968e1dc37271c50f7ec1cbed87ce77184
Katerina Sofroniou <katerina.sof@btinternet.com>
2023-02-10 01:58:07 +0000

    feat: added detailed api to url path

--- oaxaca/menu/urls.py
+++ oaxaca/menu/urls.py
@@ -1,6 +1,7 @@
-from django.urls import path, include
-from .views import DishApiView
+from django.urls import path
+from .views import DishApiView, DishDetailApiView
 
 urlpatterns = [
     path('api', DishApiView.as_view()),
+    path('api/<string:dishName>/', DishDetailApiView.as_view())
 ]
