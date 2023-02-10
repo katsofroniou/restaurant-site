7d018dda2fc28e1bbec8b480b235d998b10ce1d0
Katerina Sofroniou <katerina.sof@btinternet.com>
2023-02-10 02:22:58 +0000

    bug: fixed api/dish path

--- oaxaca/menu/urls.py
+++ oaxaca/menu/urls.py
@@ -3,5 +3,5 @@ from .views import DishApiView, DishDetailApiView
 
 urlpatterns = [
     path('api', DishApiView.as_view()),
-    path('api/<string:dishName>/', DishDetailApiView.as_view())
+    path('api/<dishVal>/', DishDetailApiView.as_view())
 ]
