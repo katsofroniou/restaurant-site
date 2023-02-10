24695ad175e38fba767bbe229b17d7bfaf2883a9
zkac123 <Natalia.Widmann.2021@live.rhul.ac.uk>
2023-02-10 02:38:23 +0000

    feat:added add to menu form

--- frontend/src/pages/AddItem.jsx
+++ frontend/src/pages/AddItem.jsx
@@ -5,9 +5,34 @@ import "../styling/AddItem.css";
 function AddItem () {
     return (
         <>
-            <button id="add_button">
-                <Link to='/waiter' id="button_link">Add Item to Menu</Link>
-            </button>
+            <div class="torso">
+                <div class="form_container">
+                    <form id="login">
+                        <div class="form_message form_message-error"></div>
+                        <div class="form_input-group">
+                            <input type="text" class="form_input" autofocus placeholder="Type" />
+                        </div>
+                        <div class="form_input-group">
+                            <input type="text" class="form_input" autofocus placeholder="Description" />
+                        </div>
+                        <div class="form_input-group">
+                            <input type="number" class="form_input" autofocus placeholder="Calories" />
+                        </div>
+                        <div class="form_input-group">
+                            <input type="text" class="form_input" autofocus placeholder="Vegan/Vegetarian" />
+                        </div>
+                        <div class="form_input-group">
+                            <input type="text" class="form_input" autofocus placeholder="Allergens" />
+                        </div>
+                        <div class="form_input-group">
+                            <input type="text" class="form_input" autofocus placeholder="Cost" />
+                        </div>
+                        <button id="add_button">
+                            <Link to='/waiter' id="button_link">Add Item to Menu</Link>
+                        </button>
+                    </form>
+                </div>
+            </div>
         </>
     );
 }
