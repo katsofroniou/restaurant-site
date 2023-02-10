8cd9001039602fbd925958fce568d9da47660a0d
zkac123 <Natalia.Widmann.2021@live.rhul.ac.uk>
2023-02-10 02:21:56 +0000

    feat:Added button + styling to add item

--- frontend/src/pages/AddItem.jsx
+++ frontend/src/pages/AddItem.jsx
@@ -4,7 +4,9 @@ import "../styling/AddItem.css";
 function AddItem () {
     return (
         <>
-
+            <button id="add_button">
+                <Link to='/waiter' id="button_link">Add Item to Menu</Link>
+            </button>
         </>
     );
 }
