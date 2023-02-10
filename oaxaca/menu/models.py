e627cd1cb09299b3ac2070a38d67b8064ca3b6d8
Katerina Sofroniou <katerina.sof@btinternet.com>
2023-02-10 00:26:02 +0000

    update: added better help text to models

--- oaxaca/menu/models.py
+++ oaxaca/menu/models.py
@@ -14,7 +14,7 @@ class Allergen(models.Model):
         ('Soybeans', 'Soybeans'),
     ]
     
-    allergen = models.CharField(max_length=50, help_text="Enter the allergen", choices=ALLERGENS_CHOICES, blank=True)
+    allergen = models.CharField(max_length=50, choices=ALLERGENS_CHOICES, blank=True)
 
     def __str__(self):
         return self.allergen
@@ -25,15 +25,15 @@ class Dish(models.Model):
         (False, 'No'),
     ]
     
-    name = models.CharField(max_length=100, help_text="Enter the item's name")
-    description = models.CharField(max_length=500, help_text="Write a description of the item")
-    allergens = models.ManyToManyField(Allergen, blank=True)
-    kcal = models.IntegerField(help_text="Enter the calories of this item")
-    course = models.CharField(max_length=25, help_text="What course this item is for")
-    price = models.FloatField(help_text="How much does this item cost?")
-    vegetarian = models.BooleanField(choices=BOOLEAN_CHOICES, help_text="Is this item vegetarian?")
-    vegan = models.BooleanField(choices=BOOLEAN_CHOICES, help_text="Is this item vegan?")
-    available = models.BooleanField(choices=BOOLEAN_CHOICES, help_text= "Is this item in stock" )
+    name = models.CharField(max_length=100)
+    description = models.CharField(max_length=500)
+    allergens = models.CharField(max_length=25, blank=True)
+    kcal = models.IntegerField(help_text="e.g. 1200")
+    course = models.CharField(max_length=25, help_text="Main, side, dessert, drink")
+    price = models.FloatField(help_text="e.g. 2.99")
+    vegetarian = models.BooleanField(choices=BOOLEAN_CHOICES, help_text="Yes / No")
+    vegan = models.BooleanField(choices=BOOLEAN_CHOICES, help_text="Yes / No")
+    available = models.BooleanField(choices=BOOLEAN_CHOICES, help_text= "Yes / No" )
     
     def __str__(self):
         return self.name
