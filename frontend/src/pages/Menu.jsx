542e12a12f2774759e47bd5ef9d817065de863df
zkac123 <Natalia.Widmann.2021@live.rhul.ac.uk>
2023-02-10 01:44:57 +0000

    fix: fixed spelling of calories

--- frontend/src/pages/Menu.jsx
+++ frontend/src/pages/Menu.jsx
@@ -4,7 +4,7 @@ import "../styling/Menu.css";
 function Menu () {
     return (
         <>
-            <button class="menu_button">Show Callories and Allergens</button>
+            <button class="menu_button">Show Calories and Allergens</button>
             <button class="menu_button">call waiter</button>
             <table class="menu_table">
                 <tr>
