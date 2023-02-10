676fad2611f3784350e6aa5198499b9439272007
Katerina Sofroniou <katerina.sof@btinternet.com>
2023-02-10 02:00:35 +0000

    feat: created dish detail api view

--- oaxaca/menu/views.py
+++ oaxaca/menu/views.py
@@ -68,3 +68,56 @@ class DishApiView(APIView):
         
         else:
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        
+class DishDetailApiView(APIView):
+    permission_classes =  [permissions.IsAuthenticated]
+    
+    def get_object(self, request, dishVal, *args, **kwargs):
+        try:
+            return Dish.objects.get(name=dishVal)
+        except:
+            return None
+    
+    def put(self, request, dishVal, *args, **kwargs):
+        dish = self.get_object(dishVal)
+        
+        if not dish:
+            return Response (
+                {"res": "Dish with this name does not exist"},
+                status = status.HTTP_400_BAD_REQUEST
+            )
+            
+        data = {
+            'name': request.data.get('name'),
+            'description': request.data.get('description'),
+            'allergens': request.data.get('allergens'),
+            'kcal': request.data.get('kcal'),
+            'course': request.data.get('course'),
+            'price': request.data.get('price'),
+            'vegetarian': request.data.get('vegetarian'),
+            'vegan': request.data.get('vegan'),
+            'available': request.data.get('available')
+        }
+        
+        serializer = DishSerializer(instance= dish, data=data, partial=True)
+        
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status= status.HTTP_200_OK)
+        
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    def delete(self, request, dishVal, *args, **kwargs):
+        dish_instance = self.get_object(dishVal)
+        
+        if not dish_instance:
+            return Response(
+                {"res": "Dish with this name does not exist"},
+                status=status.HTTP_400_BAD_REQUEST   
+            )
+        
+        dish_instance.delete()
+        return Response(
+            {"res": "Dish deleted!"},
+            status=status.HTTP_200_OK
+        )
