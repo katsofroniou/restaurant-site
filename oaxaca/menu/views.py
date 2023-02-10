3b79194b6cb18dc67373a52599b8c59818f50649
Katerina Sofroniou <katerina.sof@btinternet.com>
2023-02-10 02:21:34 +0000

    fix: removed unusable methods

--- oaxaca/menu/views.py
+++ oaxaca/menu/views.py
@@ -7,45 +7,12 @@ from .serializers import DishSerializer
 class DishApiView(APIView):
     permission_classes = [permissions.IsAuthenticated]
     
-    def getCourse(self, request, *arg, **kwargs):
-        dishCourse = Dish.objects.filter(dish = request.Dish.course)
-        serializer = DishSerializer(dishCourse, many=True)
-        return Response(serializer.data, status.HTTP_200_OK)
+    def get(self, request, *args, **kwargs):
+        dishes = Dish.objects
+        serializer = DishSerializer(dishes, many=True)
+        
+        return Response(serializer.data, status=status.HTTP_200_OK)
     
-    def getAllergens(self, request, *arg, **kwargs):
-        dishAllergens = Dish.objects.filter(dish = request.Dish.allergens)
-        serializer = DishSerializer(dishAllergens, many=True)
-        return Response(serializer.data, status.HTTP_200_OK)
-    
-    def getPrice(self, request, *arg, **kwargs):
-        dishPrice = Dish.objects.filter(dish = request.Dish.price)
-        serializer = DishSerializer(dishPrice, many=True)
-        return Response(serializer.data, status.HTTP_200_OK)
-    
-    def getName(self, request, *arg, **kwargs):
-        dishName = Dish.objects.filter(dish = request.Dish.name)
-        serializer = DishSerializer(dishName, many=True)
-        return Response(serializer.data, status.HTTP_200_OK)
-    
-    def getVegetarian(self, request, *arg, **kwargs):
-        dishVeg = Dish.objects.filter(dish = request.Dish.vegetarian)
-        serializer = DishSerializer(dishVeg, many=True)
-        return Response(serializer.data, status.HTTP_200_OK)
-    
-    def getVegan(self, request, *arg, **kwargs):
-        dishVeg = Dish.objects.filter(dish = request.Dish.vegan)
-        serializer = DishSerializer(dishVeg, many=True)
-        return Response(serializer.data, status.HTTP_200_OK)
-    
-    def getKcal(self, request, *arg, **kwargs):
-        dishKcal = Dish.objects.filter(dish = request.Dish.kcal)
-        serializer = DishSerializer(dishKcal, many=True)
-        return Response(serializer.data, status.HTTP_200_OK)
-    
-    def getAvailable(self, request, *arg, **kwargs):
-        dishAvailable = Dish.objects.filter(dish = request.Dish.available)
-        serializer = DishSerializer(dishAvailable, many=True)
-        return Response(serializer.data, status.HTTP_200_OK)
     
     def post(self, request, *args, **kwargs):
         data = {
@@ -72,12 +39,13 @@ class DishApiView(APIView):
 class DishDetailApiView(APIView):
     permission_classes =  [permissions.IsAuthenticated]
     
-    def get_object(self, request, dishVal, *args, **kwargs):
+    def get_object(self, dishVal, *args, **kwargs):
         try:
             return Dish.objects.get(name=dishVal)
         except:
             return None
-    
+        
+  
     def put(self, request, dishVal, *args, **kwargs):
         dish = self.get_object(dishVal)
         
