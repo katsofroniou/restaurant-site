c7e11d304dbadd302876e62f4ddbb3ec960fe52d
zkac123 <Natalia.Widmann.2021@live.rhul.ac.uk>
2023-02-10 01:53:12 +0000

    feat: added menu to waiter page

--- frontend/src/pages/Waiter.jsx
+++ frontend/src/pages/Waiter.jsx
@@ -4,6 +4,24 @@ import "../styling/Waiter.css";
 function Waiter () {
     return (
         <>
+            <table class="menu_table">
+                <tr>
+                    <th>Type</th>
+                    <th>Description</th>
+                    <th>Calories</th>
+                    <th>Vegan/Vegetarian</th>
+                    <th>Allergens</th>
+                    <th>Cost</th>
+                </tr>
+                <tr>
+                    <td>Empty Item</td>
+                    <td>Empty Description</td>
+                    <td>Empty Calories</td>
+                    <td>Empty Vegan</td>
+                    <td>Empty Allergens</td>
+                    <td>Empty Cost</td>  
+                </tr>
+            </table>
             <button id="addButton">Add to Menu</button>
         </>
     );
