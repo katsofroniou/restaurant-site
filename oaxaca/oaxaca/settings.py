27aa8181a44856bc48f8efb55bfd647e0de55968
Katerina Sofroniou <katerina.sof@btinternet.com>
2023-02-11 04:59:11 +0000

    wip: set up cors headers for use

--- oaxaca/oaxaca/settings.py
+++ oaxaca/oaxaca/settings.py
@@ -34,6 +34,10 @@ INSTALLED_APPS = [
     'corsheaders',
 ]
 
+CORS_ORIGIN_WHITELIST = (
+    'http://localhost:3000',
+)
+
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.AllowAny',
@@ -48,6 +52,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'oaxaca.urls'
