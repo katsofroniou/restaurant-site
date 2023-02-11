8fb5b22af43d41b6ff94cf819ec0570cbefb72dd
Katerina Sofroniou <katerina.sof@btinternet.com>
2023-02-11 20:49:43 +0000

    fix: made allergen the pk in allergen table

--- oaxaca/menu/models.py
+++ oaxaca/menu/models.py
@@ -14,7 +14,7 @@ class Allergen(models.Model):
         ('Soybeans', 'Soybeans'),
     ]
     
-    allergen = models.CharField(max_length=50, choices=ALLERGENS_CHOICES, blank=True)
+    allergen = models.CharField(max_length=50, choices=ALLERGENS_CHOICES, blank=True, primary_key=True)
 
     def __str__(self):
         return self.allergen
@@ -27,7 +27,7 @@ class Dish(models.Model):
     
     name = models.CharField(max_length=100)
     description = models.CharField(max_length=500)
-    allergens = models.CharField(max_length=25, blank=True)
+    allergens = models.ManyToManyField(Allergen, blank=True)
     kcal = models.IntegerField(help_text="e.g. 1200")
     course = models.CharField(max_length=25, help_text="Main, side, dessert, drink")
     price = models.FloatField(help_text="e.g. 2.99")
