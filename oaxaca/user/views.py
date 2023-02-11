79789da87c7ba4a01953c41876fc558d090c2068
Katerina Sofroniou <katerina.sof@btinternet.com>
2023-02-11 22:48:37 +0000

    feat: created user api view

--- oaxaca/user/views.py
+++ oaxaca/user/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status, permissions
+from .models import User
+from .serializers import UserSerializer
 
-# Create your views here.
+class UserApiView(APIView):
+    permission_classes = [permissions.IsAuthenticated]
+    
+    def get(self, *args, **kwargs):
+        users = User.objects
+        serializer = UserSerializer(users, many=True)
+        
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    
+    def post(self, request, *args, **kwargs):
+        data = {
+            'first_name': request.data.get('first_name'),
+            'last_name': request.data.get('last_name'),
+            'username': request.data.get('username'),
+            'password': request.data.get('password'),
+            'email': request.data.get('email')
+        }
+        
+        serializer = UserSerializer(data=data)
+        
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
