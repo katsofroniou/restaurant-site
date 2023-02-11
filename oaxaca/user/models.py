9a3bd492b8cf8943d9262aac541a4d0800def7cb
Katerina Sofroniou <katerina.sof@btinternet.com>
2023-02-11 22:56:49 +0000

    fix: removed unknown min_length field

--- oaxaca/user/models.py
+++ oaxaca/user/models.py
@@ -4,7 +4,7 @@ class User(models.Model):
     first_name = models.CharField(max_length=50, help_text="John")
     last_name = models.CharField(max_length=75, help_text="Doe")
     username = models.CharField(max_length=20, help_text="jdoe", unique=True, primary_key=True)
-    password = models.CharField(min_length=8, max_length=15, help_text="Johnpassword!")
+    password = models.CharField(max_length=15, help_text="Johnpassword!")
     email = models.EmailField(help_text="example@oaxaca.com", unique=True)
     
     def __str__(self):
