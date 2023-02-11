d2e37081016d0c067cf8294d4c901f371577084a
Katerina Sofroniou <katerina.sof@btinternet.com>
2023-02-11 21:08:24 +0000

    chore: changed folder name to user

--- oaxaca/user/apps.py
+++ oaxaca/user/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class UserApiConfig(AppConfig):
+class UserConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'user_api'
+    name = 'user'
