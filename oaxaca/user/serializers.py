48afbb3acacb81f0b6b36e6fc3ee839b531ce846
Katerina Sofroniou <katerina.sof@btinternet.com>
2023-02-12 19:40:27 +0000

    fix: updated fields in serializer

--- oaxaca/user/serializers.py
+++ oaxaca/user/serializers.py
@@ -4,4 +4,5 @@ from .models import User
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ["first_name", "last_name", "username", "password", "email"]
+        fields = ["first_name", "last_name", "username", "password", \
+            "waiter_permissions", "kitchen_permissions", "admin_permissions","email"]
