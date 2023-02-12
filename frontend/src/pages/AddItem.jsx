4396119576fc691c5f34c98bb2b6246221a667f8
Katerina Sofroniou <katerina.sof@btinternet.com>
2023-02-12 19:41:32 +0000

    feat: updated form fields and connected to django api
    Items in form are now sent to the database and stored

--- frontend/src/pages/AddItem.jsx
+++ frontend/src/pages/AddItem.jsx
@@ -1,38 +1,94 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from 'axios';
 import { Link } from 'react-router-dom';
 import "../styling/AddItem.css";
 
 function AddItem () {
+    const [formData, setFormData] = useState({
+        name: '',
+        description: '',
+        kcal: '',
+        vegetarian: '',
+        vegan: '',
+        allergens: '',
+        price: '',
+        available: ''
+      });      
+
+    const handleChange = event => {
+        setFormData({
+            ...formData,
+            [event.target.name]: event.target.value
+        });
+    };
+      
+    const handleSubmit = async event => {
+        event.preventDefault();
+        console.log(formData);
+    
+        const allergensArray = formData.allergens.split(',').map(allergen => allergen.trim().charAt(0).toUpperCase() + allergen.trim().slice(1));
+        const kcal = parseInt(formData.kcal);
+        const price = parseFloat(formData.price);
+        const vegetarian = formData.vegetarian.toLowerCase().trim() === 'y' || formData.vegetarian.toLowerCase().trim() === 'yes';
+        const vegan = formData.vegan.toLowerCase().trim() === 'y' || formData.vegan.toLowerCase().trim() === 'yes';
+        const available = formData.available.toLowerCase().trim() === 'y' || formData.available.toLowerCase().trim() === 'yes';
+    
+        const formDataWithCorrectTypes = {
+          ...formData,
+          allergens: allergensArray,
+          kcal,
+          price,
+          vegetarian,
+          vegan,
+          available
+        };
+
+        console.log(formDataWithCorrectTypes);
+
+        try {
+            const headers = {
+                'Content-Type': 'application/json'
+            };
+            const response = await axios.post('http://127.0.0.1:8000/menu/api', formDataWithCorrectTypes, { headers });
+            console.log(response.data);
+        } catch (error) {
+            console.error(error);
+        }
+    };      
+
     return (
         <>
             <div class="torso">
                 <div class="form_container">
-                    <form id="login">
+                    <form id="login" onSubmit={handleSubmit}>
                         <div class="form_input-group">
-                            <input type="text" class="form_input" autofocus placeholder="Type" />
+                            <input type="text" class="form_input" autoFocus name="name" value={formData.name} onChange={handleChange} placeholder="Name" />
                         </div>
                         <div class="form_input-group">
-                            <input type="text" class="form_input" autofocus placeholder="Description" />
+                            <input type="text" class="form_input" autoFocus name="description" value={formData.description} onChange={handleChange} placeholder="Description" />
                         </div>
                         <div class="form_input-group">
-                            <input type="number" class="form_input" autofocus placeholder="Calories" />
+                            <input type="number" class="form_input" autoFocus name="kcal" value={formData.kcal} onChange={handleChange}  placeholder="Calories" />
                         </div>
                         <div class="form_input-group">
-                            <input type="text" class="form_input" autofocus placeholder="Vegetarian (y/n)" />
+                            <input type="text" class="form_input" autoFocus name="vegetarian" value={formData.vegetarian} onChange={handleChange}  placeholder="Vegetarian (y/n)" />
                         </div>
                         <div class="form_input-group">
-                            <input type="text" class="form_input" autofocus placeholder="Vegan (y/n)"/>
+                            <input type="text" class="form_input" autoFocus name="vegan" value={formData.vegan} onChange={handleChange} placeholder="Vegan (y/n)"/>
                         </div>
                         <div class="form_input-group">
-                            <input type="text" class="form_input" autofocus placeholder="Allergens" />
+                            <input type="text" class="form_input" autoFocus name="allergens" value={formData.allergens} onChange={handleChange} placeholder="Allergens" />
                         </div>
                         <div class="form_input-group">
-                            <input type="text" class="form_input" autofocus placeholder="Cost" />
+                            <input type="text" class="form_input" autoFocus name="price" value={formData.price} onChange={handleChange} placeholder="Cost" />
                         </div>
                         <div class="form_input-group">
-                            <input type="" class="form_input" autofocus placeholder="Available (y/n)" />
+                            <input type="text" class="form_input" autoFocus name="course" value={formData.course} onChange={handleChange} placeholder="Course" />
                         </div>
-                        <button id="add_button">
+                        <div class="form_input-group">
+                            <input type="" class="form_input" autoFocus name="available" value={formData.available} onChange={handleChange} placeholder="Available (y/n)" />
+                        </div>
+                        <button id="add_button" onClick={handleSubmit}>
                             <Link to='/waiter' id="button_link">Add Item to Menu</Link>
                         </button>
                     </form>
