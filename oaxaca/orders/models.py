120a523408b722c96680210265e5b3979c37f4ab
zkac189 <zkac189@live.rhul.ac.uk>
2023-02-14 18:06:45 +0000

    feat: creating the database to store orders

--- oaxaca/orders/models.py
+++ oaxaca/orders/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from menu.models import Dish
 
-# Create your models here.
+#database for storing orders
+class Order(models.Model):
+    BOOLEAN_CHOICES = [
+        (True, 'Yes'),
+        (False, 'No'),
+    ]
+
+    items = models.ManyToManyField(Dish)
+    confirmed = models.BooleanField(choices=BOOLEAN_CHOICES, help_text="Yes / No")
+    
+    def __str__(self):
+        return self.name
+
+
+class Meta:
+    ordering = ['id']
