7ac74738a742c282fadee9a2895d954949ffdb4b
zkac189 <zkac189@live.rhul.ac.uk>
2023-02-15 15:24:34 +0000

    feat: basic template for the views.py

--- oaxaca/orders/views.py
+++ oaxaca/orders/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status, permissions
+from .models import Order
+from .serializers import OrderSerializer
 
-# Create your views here.
+class OrderApiView(APIView):
+    def get(self, request, *args, **kwards):
+
+    def post(self, request, *args, **kwargs):
+        
+    
+
+class OrderDetailApiView(APIView):
+    def get_object(self, OrderVal, *args **kwargs):
+
+    def get(self, OrderVal, *args, **kwargs):
+
+    def put(self, requst, OrderVal, *args, **kwargs):
+    
+    def delete(self, OrderVal, *args, **kwargs):
