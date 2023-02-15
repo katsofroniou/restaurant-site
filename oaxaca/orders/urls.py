753bf07d3eba74c06de2cf6ec635b3be94b0a3e6
zkac189 <zkac189@live.rhul.ac.uk>
2023-02-15 18:29:36 +0000

    fix: changed letter to captial to match in views

--- oaxaca/orders/urls.py
+++ oaxaca/orders/urls.py
@@ -3,5 +3,5 @@ from .views import OrderApiView, OrderDetailApiView
 
 urlpatterns = [
     path('api', OrderApiView.as_view()),
-    path('api/<orderVal>/', OrderDetailApiView.as_view())
+    path('api/<OrderVal>/', OrderDetailApiView.as_view())
 ]
