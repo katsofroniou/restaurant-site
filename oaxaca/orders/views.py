5cab328fecb4c2da305b30f49e68a17c3d2d7d7a
zkac189 <zkac189@live.rhul.ac.uk>
2023-02-15 17:00:35 +0000

    fix: added back code that was deleted

--- oaxaca/orders/views.py
+++ oaxaca/orders/views.py
@@ -5,9 +5,12 @@ from .models import Order
 from .serializers import OrderSerializer
 from django.urls import reverse
 
-
 class OrderApiView(APIView):
+    permission_classes = [permissions.AllowAny]
+
     def get(self, request, *args, **kwards):
+        orders = Order.objects
+        serializer = OrderSerializer(orders, many=True)
 
     def post(self, request, *args, **kwargs):
         data = {
@@ -21,6 +24,9 @@ class OrderApiView(APIView):
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status = status.HTTP_201_CREATED)
+        
+        else:
+            return Response(serializer.errors, status = status.HTTP_400_BAD_REQUEST)
         
 
 class OrderDetailApiView(APIView):
