defa21260936145bcd646542b626364d50725194
Katerina Sofroniou <katerina.sof@btinternet.com>
2023-02-16 23:52:46 +0000

    feat: changed model for User table

--- oaxaca/user/models.py
+++ oaxaca/user/models.py
@@ -1,9 +1,16 @@
 from django.db import models
 
 class User(models.Model):
-    username = models.CharField(max_length=20, help_text="jdoe", unique=True, primary_key=True)
-    first_name = models.CharField(help_text="John")
-    email = models.EmailField(help_text="email@example.com")
+    PERMISSION_CHOICES = [
+        ("customer_permissions","customer_permissions"),
+        ("waiter_permissions","waiter_permissions"),
+        ("kitchen_permissions","kitchen_permissions")
+    ]
+    
+    username = models.ForeignKey("People.model", on_delete=models.CASCADE, primary_key=True)
+    password = models.CharField(help_text="password", nullable=False)
+    permissions = models.CharField(choices=PERMISSION_CHOICES, nullable=False)
+    login = models.BooleanField(nullable=False)
     
     def __str__(self):
         return self.username
