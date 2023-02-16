29092ac30b40bddaf578a67819f7d73ed86a70de
Katerina Sofroniou <katerina.sof@btinternet.com>
2023-02-16 23:45:27 +0000

    feat: created api for People

--- oaxaca/people/views.py
+++ oaxaca/people/views.py
@@ -1,3 +1,31 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status, permissions
+from .models import People
+from .serializers import PeopleSerializer
 
-# Create your views here.
+class PeopleApiView(APIView):
+    permission_classes = [permissions.AllowAny]
+    
+    def get(self, request, *args, **kwargs):
+        people = People.objects
+        serializer = PeopleSerializer(people, many=True)
+        
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    
+    def post(self, request, *args, **kwargs):
+        data = {
+            'username': request.data.get('username'),
+            'first_name': request.data.get('first_name'),
+            'last_name': request.data.get('last_name'),
+            'email': request.data.get('email')
+        }
+        
+        serializer = PeopleSerializer(data=data)
+        
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
