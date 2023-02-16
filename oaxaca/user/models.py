33e8c2a38569b6be4fa55cd047432081d332bbdc
Katerina Sofroniou <katerina.sof@btinternet.com>
2023-02-16 22:53:04 +0000

    feat: added first-name field to user model

--- oaxaca/user/models.py
+++ oaxaca/user/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class User(models.Model):
     username = models.CharField(max_length=20, help_text="jdoe", unique=True, primary_key=True)
-    password = models.CharField(max_length=15, help_text="Johnpassword!")
+    first_name = models.CharField(help_text="John")
     email = models.EmailField(help_text="email@example.com")
     
     def __str__(self):
