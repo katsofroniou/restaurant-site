460c034a2877f0b15380da7ba1ef372f13e0233e
Katerina Sofroniou <katerina.sof@btinternet.com>
2023-02-16 23:42:16 +0000

    feat: created model for People table

--- oaxaca/people/models.py
+++ oaxaca/people/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class People(models.Model):
+    username = models.CharField(nullable=False, primary_key=True)
+    first_name = models.CharField(nullable=False)
+    last_name = models.CharField(nullable=False)
+    email = models.EmailField(nullable=False)
+    
+    def __str__(self):
+        return self.username
+    
+class Meta:
+    people = ['username']
