30083cf4a62fdfbbf5448cadfb25676b09be1a87
Katerina Sofroniou <katerina.sof@btinternet.com>
2023-02-17 00:01:43 +0000

    fix: added max_length to fields

--- oaxaca/people/models.py
+++ oaxaca/people/models.py
@@ -2,9 +2,9 @@ from django.db import models
 
 # Create your models here.
 class People(models.Model):
-    username = models.CharField(null=False, primary_key=True)
-    first_name = models.CharField(null=False)
-    last_name = models.CharField(null=False)
+    username = models.CharField(null=False, primary_key=True, max_length=255)
+    first_name = models.CharField(null=False, max_length=25)
+    last_name = models.CharField(null=False, max_length=255)
     email = models.EmailField(null=False)
     
     def __str__(self):
