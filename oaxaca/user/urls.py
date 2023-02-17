88893a882569c02da044f81abc4ad07bf9c413cd
Katerina Sofroniou <katerina.sof@btinternet.com>
2023-02-17 02:16:20 +0000

    chore: changed URL paths

--- oaxaca/user/urls.py
+++ oaxaca/user/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from .views import UserApiView, UserDetailApiView, PeopleApiView, PeopleDetailedApiView
 
 urlpatterns = [
-    path('user/api', UserApiView.as_view()),
-    path('user/api/<username>/', UserDetailApiView.as_view()),
-    path('people/api', PeopleApiView.as_view()),
-    path('people/api/<username>', PeopleDetailedApiView.as_view())
+    path('api', UserApiView.as_view()),
+    path('api/<username>/', UserDetailApiView.as_view()),
+    path('person/api', PeopleApiView.as_view()),
+    path('person/api/<username>', PeopleDetailedApiView.as_view())
 ]
