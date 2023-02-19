5608415b8599f41370984f7f601b95f9d9f7d341
Katerina Sofroniou <katerina.sof@btinternet.com>
2023-02-19 02:48:03 +0000

    fix: fixed spelling issue in IsAuthenticated

--- oaxaca/oaxaca/settings.py
+++ oaxaca/oaxaca/settings.py
@@ -41,9 +41,9 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.TokenAuthentication',
     ),
-    'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.isAuthenticated'
-    ]
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated'
+    )
 }
 
 DJOSER = {
