6c0e3a7f1b703f751f16660cf0d015342873cebe
Katerina Sofroniou <katerina.sof@btinternet.com>
2023-02-19 02:19:44 +0000

    chore: removed user app

--- oaxaca/user/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import UserApiView, UserDetailApiView, PeopleApiView, PeopleDetailedApiView
-
-urlpatterns = [
-    path('api', UserApiView.as_view()),
-    path('api/<username>/', UserDetailApiView.as_view()),
-    path('person/api', PeopleApiView.as_view()),
-    path('person/api/<username>', PeopleDetailedApiView.as_view())
-]
