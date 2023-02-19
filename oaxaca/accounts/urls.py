aec26ef917809fe6455cebc30cfdb42f87d5635b
Katerina Sofroniou <katerina.sof@btinternet.com>
2023-02-19 02:33:57 +0000

    fix: removed r'^ from account api path

--- oaxaca/accounts/urls.py
+++ oaxaca/accounts/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls import include
 from django.urls import path
 
 accounts_urlpatterns = [
-    path(r'^api/v1/', include('djoser.urls')),
-    path(r'^api/v1/', include('djoser.urls.authtoken')),
+    path('api/v1/', include('djoser.urls')),
+    path('api/v1/', include('djoser.urls.authtoken')),
 ]
