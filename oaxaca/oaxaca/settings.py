773911ad4a444fc99fd80da98011e03915818b13
Katerina Sofroniou <katerina.sof@btinternet.com>
2023-02-20 16:33:13 +0000

    fix: removed old partial authentication code

--- oaxaca/oaxaca/settings.py
+++ oaxaca/oaxaca/settings.py
@@ -29,26 +29,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'rest_framework.authtoken',
     'menu',
     'orders',
-    'corsheaders',
-    'djoser',
-    'accounts',
 ]
-
-REST_FRAMEWORK = {
-    'DEFAULT_AUTHENTICATION_CLASSES': (
-        'rest_framework.authentication.TokenAuthentication',
-    ),
-    'DEFAULT_PERMISSION_CLASSES': (
-        'rest_framework.permissions.IsAuthenticated'
-    )
-}
-
-DJOSER = {
-    "USER_ID_FIELD": "username"
-}
 
 CORS_ORIGIN_WHITELIST = (
     'http://localhost:3000',
@@ -56,6 +39,7 @@ CORS_ORIGIN_WHITELIST = (
 )
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -63,7 +47,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'oaxaca.urls'
