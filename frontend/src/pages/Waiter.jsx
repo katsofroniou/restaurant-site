88a11cbfdbe9a72c39d797c6380388a2a9c5136d
zkac123 <Natalia.Widmann.2021@live.rhul.ac.uk>
2023-02-20 17:40:29 +0000

    feat: added the menu pulled from database to the waiter page

--- frontend/src/pages/Waiter.jsx
+++ frontend/src/pages/Waiter.jsx
@@ -1,13 +1,27 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from 'react-router-dom';
 import "../styling/Waiter.css";
+import axios from 'axios';
 
 function Waiter () {
+    const[dish, SetDish] = useState([])
+
+    const getDish = async () => {
+        const response = await axios.get('http://127.0.0.1:8000/menu/api')
+        SetDish(response.data)
+    }
+
+    useEffect(() =>{
+        getDish();
+    }, [])
+    
     return (
         <>
+            
             <table class="menu_table">
                 <tr>
-                    <th>Type</th>
+                    <th>Course</th>
+                    <th>Dish</th>
                     <th>Description</th>
                     <th>Calories</th>
                     <th>Vegetarian</th>
@@ -17,19 +31,23 @@ function Waiter () {
                     <th>Available</th>
                     <th>Delete?</th>
                 </tr>
-                <tr>
-                    <td>Empty Item</td>
-                    <td>Empty Description</td>
-                    <td>Empty Calories</td>
-                    <td>Empty Vegetarian</td>
-                    <td>Empty Vegan</td>
-                    <td>Empty Allergens</td>
-                    <td>Empty Cost</td>
-                    <td>Empty Availability</td>
-                    <td>
-                        <input type="checkbox"></input>
-                    </td>  
-                </tr>
+                { dish.map((dish, index) =>(
+                    <tr>
+                        <td>{dish.course}</td>
+                        <td>{dish.name}</td>
+                        <td>{dish.description}</td>
+                        <td>{dish.kcal}</td>
+                        {dish.vegetarian === true && <td>Yes</td>}
+                        {dish.vegetarian === false && <td>No</td>}
+                        {dish.vegan === true && <td>Yes</td>}
+                        {dish.vegan === false && <td>No</td>}
+                        <td>{dish.allergens.join(", ")}</td>
+                        <td>£{dish.price.toLocaleString("en-GB", {minimumFractionDigits:2, maximumFractionDigits:2})}</td>
+                        {dish.available === true && <td>Yes</td>}
+                        {dish.available === false && <td>No</td>}
+                        <td><input type="checkbox"></input></td>
+                    </tr>
+                ))}
             </table>
             <button class="add_button">
                 <Link to='/additem' class="button_link">Add to Menu</Link>
