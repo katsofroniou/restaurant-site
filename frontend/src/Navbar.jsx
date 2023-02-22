9432ce6d60fd4a46c83fd70011e93dffb17e9e13
Katerina Sofroniou <katerina.sof@btinternet.com>
2023-02-22 19:35:54 +0000

    feat: Navbar changes according to permissions

--- frontend/src/Navbar.jsx
+++ frontend/src/Navbar.jsx
@@ -5,29 +5,60 @@ import axios from 'axios';
 import "./styling/Navbar.css";
 import Logo from '../src/Images/OaxacaLogo.png'
 
-function Navbar () {
-    const [user, setUser] = useState([]);
+function Navbar() {
+    const token = localStorage.getItem('access_token');
+    const [groups, setGroups] = useState([]);
 
-    const getUser = async () => {
-        const response = await axios.get('http://127.0.0.1:8000/user/api')
-        setUser(response.data)
-    }
+    useEffect(() => {
+        const accessToken = localStorage.getItem('access_token');
 
-    useEffect(() =>{
-        getUser();
-    }, [])
-    
+        axios.get('http://localhost:8000/@me/', {
+            headers: {
+                Authorization: `Bearer ${accessToken}`
+            }
+        })
+            .then(response => {
+                const groupNames = response.data.groups.map(group => group.name);
+                setGroups(groupNames);
+            })
+            .catch(error => {
+                console.log(error);
+            });
+    }, []);
+
+    // Define permissions for each page
+    const pagePermissions = {
+        orders: ['Waiter', 'Kitchen Staff', 'Admin'],
+        login: ['*'],
+        addItem: ['Waiter', 'Kitchen Staff'],
+        menu: ['Waiter', 'Kitchen Staff', 'Customer'],
+        waiter: ['Waiter'],
+        kitchenstaff: ['Kitchen Staff'],
+        manager: ['Admin'],
+        adduser: ['Admin']
+    };
+
+    const userHasPermission = (permission) => {
+        // Check if the user has any of the required permissions for the page
+        const allowedGroups = pagePermissions[permission];
+        if (!allowedGroups) {
+            // If the page doesn't have any specific permissions defined, allow access
+            return true;
+        }
+        return allowedGroups.some(p => groups.includes(p));
+    };
+
     return (
         <>
             <nav class="navbar">
                 <div class="navbar_container">
-                    <p> <img style = {{ width: 200, height: 110 }} src={Logo}></img> </p>
+                    <p> <img style={{ width: 200, height: 110 }} src={Logo}></img> </p>
                 </div>
                 <ul class="navbar_menu">
                     <li class="navbar_item">
                         <Link to='/' class="navbar_links">Home</Link>
                     </li>
-                    {user.permissions === "customer_permissions" && user.login === true && (
+                    {token && userHasPermission('customer') && (
                         <li class="navbar_item">
                             <Link to='/Menu' class="navbar_links">Menu</Link>
                         </li>
@@ -35,34 +66,36 @@ function Navbar () {
                     <li class="navbar_item">
                         <Link to='/Orders' class="navbar_links">Orders</Link>
                     </li>
-                    {user.permissions === "waiter_permissions" && user.login === true && (
+                    {token && userHasPermission('waiter') && (
                         <li class="navbar_item">
                             <Link to='/waiter' class="navbar_links">Waiter</Link>
                         </li>
                     )}
-                    {user.permissions === "waiter_permissions" && user.login === true && (
+                    {token && (userHasPermission('waiter') || userHasPermission('kitchenstaff') || userHasPermission('manager')) && (
                         <li class="navbar_item">
                             <Link to='/additem' class="navbar_links">Add Items</Link>
                         </li>
                     )}
-                    {user.permissions === "kitchen_permissions" && user.login === true && (
+                    {token && userHasPermission('kitchenstaff') && (
                         <li class="navbar_item">
                             <Link to='/kitchenstaff' class="navbar_links">Kitchen Staff</Link>
                         </li>
                     )}
-                    {user.permissions === "manager_permissions" && user.login === true && (
+                    {token && userHasPermission('manager') && (
                         <li class="navbar_item">
                             <Link to='/manager' class="navbar_links">Manage Users</Link>
                         </li>
                     )}
-                    {user.permissions === "manager_permissions" && user.login === true && (
+                    {token && userHasPermission('manager') && (
                         <li class="navbar_item">
                             <Link to='/adduser' class="navbar_links">Add User</Link>
                         </li>
                     )}
-                    <li class="navbar_btn">
-                        <Link to='/Login' class="navbar_button">Sign in</Link>
-                    </li>
+                    {!token && (
+                        <li class="navbar_btn">
+                            <Link to='/Login' class="navbar_button">Sign in</Link>
+                        </li>
+                    )}
                 </ul>
             </nav>
         </>
