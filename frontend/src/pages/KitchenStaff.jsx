c0964d4610634c51d8014c489b10f849d7c3bbad
Davit <zkac151@live.rhul.ac.uk>
2023-02-22 14:41:36 +0000

    fix: Made CSS for this page modular

--- frontend/src/pages/KitchenStaff.jsx
+++ frontend/src/pages/KitchenStaff.jsx
@@ -1,11 +1,11 @@
 import React from "react";
-import "../styling/KitchenStaff.css";
+import kitchenStyle from "../styling/KitchenStaff.module.css";
 
 function KitchenStaff () {
     return (
         <>
             <h1 class="header">Kitchen</h1>
-            <button class="kitchen_button" type="button">Notify Waiters</button>
+            <button class={kitchenStyle.kitchen_button} type="button">Notify Waiters</button>
 
             
         </>
