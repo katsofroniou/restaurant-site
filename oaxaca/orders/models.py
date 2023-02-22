d42e0c682bdbe1ef48e31adb2a53aada2261888b
zkac189 <zkac189@live.rhul.ac.uk>
2023-02-22 11:07:53 +0000

    feat: added a orderReady column

--- oaxaca/orders/models.py
+++ oaxaca/orders/models.py
@@ -8,14 +8,14 @@ class Order(models.Model):
         (False, 'No'),
     ]
 
-    orderTime = models.TimeField(auto_now_add=True)
+    orderTime = models.DateTimeField(auto_now_add=True)
     tableNumber = models.IntegerField(default="0")
     items = models.ManyToManyField(Dish)
     confirmed = models.BooleanField(choices=BOOLEAN_CHOICES, help_text="Yes / No")
+    orderReady = models.BooleanField(choices=BOOLEAN_CHOICES, help_text="Yes / No")
     
     def __str__(self):
-        return str(self.tableNumber)
-
+        return str(self.orderTime)
 
 class Meta:
     ordering = ['orderTime']
