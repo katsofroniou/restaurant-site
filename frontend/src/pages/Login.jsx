97bc73ef088e91f942e3b40cee7a178fc528cdf4
Katerina Sofroniou <katerina.sof@btinternet.com>
2023-02-22 15:17:55 +0000

    Revert "feat: added login functionality"
    This reverts commit 547a51a627dcd0df13afdf05ce0b175c10eb732a. Accidentally committed it to main branch.

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,81 +1,89 @@
 import react from "react";
 import React,{useState} from "react";
-import loginStyle from "../styling/Login.module.css";
+import "../styling/Login.css";
 
-function Login() {
-  const [show, setShow] = useState(false);
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
-  const [errorMessage, setErrorMessage] = useState('');
 
-  function toggleShow() {
-    setShow(!show);
-  }
+function Login(){
 
-  function handleFormSubmit(event) {
-    event.preventDefault();
-    axios.post('http://127.0.0.1:8000/token/', {
-        username: username,
-        password: password
-      })
-      .then(response => {
-        localStorage.setItem('access_token', response.data.access);
-        localStorage.setItem('refresh_token', response.data.refresh);
-        {/* 
-            const accessToken = localStorage.getItem('access_token')
-        */}
-
-        window.location.href = '/';
-      })
-      
-      .catch(error => {
-        console.error(error);
-        setErrorMessage('Invalid username or password');
-      });
-  }
-
-  function handleUsernameChange(event) {
-    setUsername(event.target.value);
-    clearInputError(event.target);
-  }
-
-  function handlePasswordChange(event) {
-    setPassword(event.target.value);
-    clearInputError(event.target);
-  }
-
-  function setInputError(inputElement, message) {
-    inputElement.classList.add("form_input_error");
-    inputElement.parentElement.querySelector(".form_input-error-message").textContent = message;
-  }
-
-  function clearInputError(inputElement) {
-    inputElement.classList.remove("form_input_error");
-    inputElement.parentElement.querySelector(".form_input-error-message").textContent = "";
-  }
-
-  return (
+    const [show,setShow] = useState();
+    var hidden = show ? "form form-hidden" : "form";
+    var hidden2 = show ? "form": "form form-hidden";
+    function toggleShow(){
+        setShow(!show);
+    }
+        
+    return (
     <>
-    <div class={loginStyle.torso}>
-        <div class={loginStyle.form_container}>
-            <form class = {loginStyle.form} id="login" onSubmit={handleFormSubmit}>
-                <h2 class={loginStyle.form_title}>Login</h2>
+    <div class="torso">
+        <div class="form_container">
+            <form class = {hidden} id="login">
+                <h2 class="form_title">Login</h2>
 
-                <div class={'${loginStyle.form_message} ${loginStyle.form_message-error}'}>{errorMessage}</div>
-                <div class={loginStyle["form_input-group"]}>
-                    <input type="text" class={loginStyle.form_input} autofocus placeholder="Username" value={username} onChange={handleUsernameChange}/>
-                    <div class={loginStyle["form_input-error-message"]}></div>
+                <div class="form_message form_message-error"></div>
+                <div class="form_input-group">
+                    <input type="text" class="form_input" autofocus placeholder="Username or Email"/>
+                    <div class="form_input-error-message"></div>
                 </div>
-                <div class={loginStyle["form_input-group"]}>
-                    <input type="password" class={loginStyle.form_input} autofocus placeholder="Password" value={password} onChange={handlePasswordChange}/>
-                    <div class={loginStyle["form_input-error-message"]}></div>
+                <div class="form_input-group">
+                    <input type="password" class="form_input" autofocus placeholder="Password"/>
+                    <div class="form_input-error-message"></div>
                 </div>
-                <button class={loginStyle.form_button} type="submit">Submit</button>
+                <button class="form_button" type="submit">Submit</button>
+                <p class="form_text">
+                    <a onClick={toggleShow} class = "form_link" id="linkCreateAccount">Sign Up?</a>
+                </p>
+                <p class="form_text">
+                    Forgot your password? Ask your employer
+                </p>
+            </form>
+
+             <form class = {hidden2} id="createAccount">
+                <h2 class="form_title">Create Account</h2>
+
+                <div class="form_message-error"></div>
+                <div class="form_input-group">
+                    <input type="text" class="form_input" autofocus placeholder="Username"/>
+                    <div class="form_input-error-message"></div>
+                </div>
+                <div class="form_input-group">
+                    <input type="text" class="form_input" autofocus placeholder="Email"/>
+                    <div class="form_input-error-message"></div>
+                </div>
+                <div class="form_input-group">
+                    <input type="password" class="form_input" autofocus placeholder="Password"/>
+                    <div class="form_input-error-message"></div>
+                </div>
+                <div class="form_input-group">
+                    <input type="text" class="form_input" autofocus placeholder="Confirm password"/>
+                    <div class="form_input-error-message"></div>
+                </div>
+                <button class="form_button" type="submit">Create Account</button>
+                <p class="form_text">
+                    <a onClick={toggleShow}  class = "form_link" id="linkLogin">Already have an account? Sign in</a>
+                </p>
             </form>
         </div>
     </div>
     </>
-  );
+
+    );
+}
+
+
+function setErrorMessage(formElement, type, message){
+    const messageElement = formElement.querySelector(".form_message");
+    messageElement.textContent=message;
+    messageElement.classList.remove("form_message-error");
+    messageElement.classList.add('form_message-$(type)');
+}
+function setInputError(inputElement, message){
+    inputElement.classList.add("form_input_error");
+    inputElement.parentElement.querySelector(".form_input-error-message").textContent = message;
+}
+
+function clearInputError(inputElement){
+    inputElement.classList.remove("form_input-error");
+    inputElement.parentElement.querySelector(".form_input-error-message").textContent = "";
 }
 
 
