9f4907ff49f379fb0e66eeb41dfc1966ca05c736
Katerina Sofroniou <katerina.sof@btinternet.com>
2023-02-22 11:34:46 +0000

    fix: removed "BOOLEAN_CHOICES"

--- oaxaca/orders/models.py
+++ oaxaca/orders/models.py
@@ -3,16 +3,11 @@ from menu.models import Dish
 
 #database for storing orders
 class Order(models.Model):
-    BOOLEAN_CHOICES = [
-        (True, 'Yes'),
-        (False, 'No'),
-    ]
-
     orderTime = models.DateTimeField(auto_now_add=True)
     tableNumber = models.IntegerField(default="0")
     items = models.ManyToManyField(Dish)
-    confirmed = models.BooleanField(choices=BOOLEAN_CHOICES, help_text="Yes / No")
-    orderReady = models.BooleanField(choices=BOOLEAN_CHOICES, help_text="Yes / No")
+    confirmed = models.BooleanField(help_text="Yes / No")
+    orderReady = models.BooleanField(help_text="Yes / No")
     
     def __str__(self):
         return str(self.orderTime)
