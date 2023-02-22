160abac6cdc6419a149f6d3be9d1e04923922e09
Katerina Sofroniou <katerina.sof@btinternet.com>
2023-02-22 21:00:44 +0000

    feat: added delete api to DishApiView

--- oaxaca/menu/views.py
+++ oaxaca/menu/views.py
@@ -36,6 +36,26 @@ class DishApiView(APIView):
         else:
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
         
+    def delete(self, request, *args, **kwargs):
+        items = request.query_params.getlist('items')
+        if not items:
+            return Response(
+                {"res": "No items to delete"},
+                status=status.HTTP_400_BAD_REQUEST
+            )
+        
+        deleted_count, _ = Dish.objects.filter(name__in=items).delete()
+        if deleted_count == 0:
+            return Response(
+                {"res": "No items deleted"},
+                status=status.HTTP_400_BAD_REQUEST
+            )
+        
+        return Response(
+            {"res": f"Deleted {deleted_count} items"},
+            status=status.HTTP_200_OK
+        )
+
 class DishDetailApiView(APIView):
     permission_classes =  [permissions.IsAuthenticatedOrReadOnly]
     
