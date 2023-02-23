e1d75c18bf9c35d6bccbffc61d1f3ba18fcd5c92
Katerina Sofroniou <katerina.sof@btinternet.com>
2023-02-23 11:12:02 +0000

    fix: fixed account api path

--- oaxaca/accounts/urls.py
+++ oaxaca/accounts/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import UserCreate
 
 urlpatterns = [
-    path('users/api', UserCreate.as_view(), name='account-create'),
+    path('api', UserCreate.as_view(), name='account-create'),
 ]
