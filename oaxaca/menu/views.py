f1272fd27525a5a8da2174adf2611da95a8fcf37
Katerina Sofroniou <katerina.sof@btinternet.com>
2023-02-24 00:24:36 +0000

    fix: removed unnecessary view from menu api

--- oaxaca/menu/views.py
+++ oaxaca/menu/views.py
@@ -121,25 +121,3 @@ class DishDetailApiView(APIView):
             {"res": "Dish deleted!"},
             status=status.HTTP_200_OK
         )
-
-class FilterMenuAPIView(APIView):
-    def get(self, request):
-        term = request.query_params.get('term', '').lower()
-        dishes = Dish.objects.filter(
-            models.Q(name__icontains=term) |
-            models.Q(description__icontains=term) |
-            models.Q(course__icontains=term) |
-            models.Q(allergens__allergen__icontains=term)
-        )
-        serialized_data = [{
-            "name": dish.name, 
-            "description": dish.description, 
-            "course": dish.course, 
-            "allergens": [allergen.allergen for allergen in dish.allergens.all()], 
-            "vegetarian": dish.vegetarian, 
-            "vegan": dish.vegan, 
-            "available": dish.available, 
-            "kcal": dish.kcal, 
-            "price": dish.price
-        } for dish in dishes]
-        return Response(serialized_data)
