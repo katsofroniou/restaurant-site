d2bdd70263026e6d94ae7ff686e319b99b0e8af0
Katerina Sofroniou <katerina.sof@btinternet.com>
2023-02-24 00:25:40 +0000

    bug: removed menu url that was causing an issue with getting single items

--- oaxaca/menu/urls.py
+++ oaxaca/menu/urls.py
@@ -1,8 +1,7 @@
 from django.urls import path
-from .views import DishApiView, DishDetailApiView, FilterMenuAPIView
+from .views import DishApiView, DishDetailApiView
 
 urlpatterns = [
     path('api', DishApiView.as_view()),
     path('api/<dishVal>/', DishDetailApiView.as_view()),
-    path('api/filter/', FilterMenuAPIView.as_view()),
 ]
