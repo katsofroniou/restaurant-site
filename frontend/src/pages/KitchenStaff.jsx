79ba71ccd53de1f83a4248f515af9945a48e351e
zkac123 <Natalia.Widmann.2021@live.rhul.ac.uk>
2023-02-27 14:58:06 +0000

    chore:standardized css

--- frontend/src/pages/KitchenStaff.jsx
+++ frontend/src/pages/KitchenStaff.jsx
@@ -1,13 +1,11 @@
 import React from "react";
-import kitchenStyle from "../styling/KitchenStaff.module.css";
+import "../styling/KitchenStaff.css";
 
 function KitchenStaff () {
     return (
         <>
-            <h1 class="header">Kitchen</h1>
-            <button class={kitchenStyle.kitchen_button} type="button">Notify Waiters</button>
-
-            
+            <h1>Kitchen</h1>
+            <button class="kitchen_button" type="button">Notify Waiters</button>
         </>
     );
 }
