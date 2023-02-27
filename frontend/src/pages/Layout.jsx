8e2a52168282c44f5573da76cdc342e7e9fac387
zkac123 <Natalia.Widmann.2021@live.rhul.ac.uk>
2023-02-27 17:06:07 +0000

    chore: added comments for clarity

--- frontend/src/pages/Layout.jsx
+++ frontend/src/pages/Layout.jsx
@@ -3,6 +3,7 @@ import { Outlet } from "react-router-dom";
 import Navbar from "../Navbar";
 import "../styling/Layout.css";
 
+//It is not necessary to touch this file. If you are in this file, you are looking in the wrong place.
 const Layout = () => {
     return (
         <>
