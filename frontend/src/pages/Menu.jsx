35dd8778d1267586d0fdabe1df0aa2854f048763
Katerina Sofroniou <katerina.sof@btinternet.com>
2023-03-01 16:29:51 +0000

    feat: added quantity column to table with changeable values

--- frontend/src/pages/Menu.jsx
+++ frontend/src/pages/Menu.jsx
@@ -5,18 +5,17 @@ import axios from 'axios';
 function Menu() {
     const [dish, setDish] = useState([])
     const [searchTerm, setSearchTerm] = useState("");
+    const [calories, setCalories] = useState(false);
 
     useEffect(() => {
         getDishFiltered();
     }, [])
 
-    const [calories, setCalories] = useState(false);
-
     const getDishFiltered = async () => {
         let url = 'http://127.0.0.1:8000/menu/api';
         const params = {};
         if (searchTerm) {
-          params['search'] = searchTerm;
+            params['search'] = searchTerm;
         }
         const response = await axios.get(url, { params });
         setDish(response.data);
@@ -30,9 +29,25 @@ function Menu() {
         setCalories((current) => !current);
     };
 
-    const handleSearch = () => {
-        getDishFiltered();
-    };
+    const [quantity, setQuantity] = useState([]);
+
+    useEffect(() => {
+        setQuantity(dish.map(() => 0));
+    }, [dish]);
+
+    const handleDecrease = (index) => {
+        if (quantity[index] > 0) {
+            const updatedQuantity = [...quantity];
+            updatedQuantity[index] = updatedQuantity[index] - 1;
+            setQuantity(updatedQuantity);
+        }
+    }
+
+    const handleIncrease = (index) => {
+        const updatedQuantity = [...quantity];
+        updatedQuantity[index] = updatedQuantity[index] + 1;
+        setQuantity(updatedQuantity);
+    }
 
     return (
         <>
@@ -56,6 +71,7 @@ function Menu() {
                     <th>Vegan/Vegetarian</th>
                     {calories && <th>Calories</th>}
                     <th>Cost</th>
+                    <th>Quantity</th>
                 </tr>
                 {dish.map((dish, index) => (
                     <tr>
@@ -68,6 +84,11 @@ function Menu() {
                         {dish.vegetarian === false && <td>N/A</td>}
                         {calories && <td>{dish.kcal}</td>}
                         <td>£{dish.price.toLocaleString("en-GB", { minimumFractionDigits: 2, maximumFractionDigits: 2 })}</td>
+                        <td>
+                            <button onClick={() => handleDecrease(index)}>-</button>
+                            <span>{quantity[index]}</span>
+                            <button onClick={() => handleIncrease(index)}>+</button>
+                        </td>
                     </tr>
                 ))}
             </table>
