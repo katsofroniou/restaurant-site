e9254334345c6f9fdc9a31b5a4dae3066aaa6549
zkac189 <zkac189@live.rhul.ac.uk>
2023-03-01 19:05:09 +0000

    feat: waiters can now delete orders by clicking cancel order

--- frontend/src/pages/Orders.jsx
+++ frontend/src/pages/Orders.jsx
@@ -24,6 +24,46 @@ function Orders() {
         }));
     };
 
+    const handleCancelClick = async () => {
+        // get access token from local storage
+        const access_token = localStorage.getItem('access_token');
+
+        //if access token is null or empty, user is not authenticated
+        if (!access_token) {
+            console.log('User not authenticated');
+            return;
+        }
+
+        // delete selected orders
+        const deleteOrders = [];
+        Object.keys(selectedRow).forEach((id) => {
+            if (selectedRow[id]) {
+                deleteOrders.push(id);
+            }
+        });
+
+        if (deleteOrders.length > 0) {
+            try {
+                await Promise.all(deleteOrders.map(order => {
+                    return axios ({
+                        method: 'DELETE',
+                        url: `http://127.0.0.1:8000/orders/api/${order}`,
+                        headers: {
+                            'Authorization': `Bearer ${access_token}`
+                        },
+                    });
+                }));
+                
+                // update state to remove deleted orders
+                const newOrder = orders.filter((d) => !deleteOrders.includes(d.id));
+                setOrders(newOrder);
+
+            } catch (error) {
+                console.error(error);
+            }
+        }
+    };
+
     return (
         <>
             <div class="order_torso">
@@ -57,7 +97,7 @@ function Orders() {
                         ))}
                     </tbody>
                 </Table>
-                <button class="order_button" type="submit" >Cancel Order</button>
+                <button class="order_button" type="submit" onClick={handleCancelClick} >Cancel Order</button>
             </div>
         </>
     );
