5c4878340089b9fd42a6a2b20594690eb223ebb2
Jubai-Khalil <jubai.khalil@gmail.com>
2023-03-01 03:22:36 +0000

    chore: added html and css styling for info boxes

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,12 +1,28 @@
 import React from "react";
 import "../styling/Home.css";
 import image from '../Images/homepage_image2.png'
+import image2 from '../Images/seating.jpeg'
+import "../styling/Home.css";
 
 
 function Home () {
     return(
         <>
             <img style={{ width: 1440, height: 860 }} src={image}></img> 
+            <div className="infoBoxContainer">
+              <div className="infoBox">
+                <img style={{ width: 300, height: 250,}} src={image2}></img> 
+                <h3 className="title">WELCOME</h3>
+              </div>
+              <div className="infoBox">
+                <img style={{ width: 300, height: 250 }} src={image2}></img> 
+                <h3 className="title">ABOUT</h3>
+              </div>
+              <div className="infoBox">
+                <img style={{ width: 300, height: 250 }} src={image2}></img> 
+                <h3 className="title">WELCOME</h3>
+              </div>
+            </div>
         </>
     );
 }
