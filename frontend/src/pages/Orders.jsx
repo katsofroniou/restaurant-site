cd598113dac44726981b835779c75915e61a5e10
zkac189 <zkac189@live.rhul.ac.uk>
2023-03-02 17:24:22 +0000

    feat: added cancel order button functionality

--- frontend/src/pages/Orders.jsx
+++ frontend/src/pages/Orders.jsx
@@ -16,6 +16,55 @@ function Orders() {
             .then(response => setOrders(response.data))
             .catch(error => console.log(error));
     }, []);
+
+    const handleOrderSelect = (selectedOrder) => {
+        setSelectedRow(prevState => ({
+            ...prevState,
+            [selectedOrder.id]: !prevState[selectedOrder.id],
+        }));
+    };
+
+    const handleCancelClick = async () => {
+        // get access token from local storage
+        const access_token = localStorage.getItem('access_token');
+
+        //if access token is null or empty, user is not authenticated
+        if (!access_token) {
+            console.log('User not authenticated');
+            return;
+        }
+
+        // delete selected orders
+        const deleteOrders = [];
+        Object.keys(selectedRow).forEach((id) => {
+            if (selectedRow[id]) {
+                deleteOrders.push(id);
+            }
+        });
+
+        if (deleteOrders.length > 0) {
+            try {
+                await Promise.all(deleteOrders.map(order => {
+                    return axios ({
+                        method: 'DELETE',
+                        url: `http://127.0.0.1:8000/orders/api/${order}`,
+                        headers: {
+                            'Authorization': `Bearer ${access_token}`
+                        },
+                    });
+                }));
+                
+                // update state to remove deleted orders
+                const newOrder = orders.filter((d) => !deleteOrders.includes(d.id));
+                setOrders(newOrder);
+
+            } catch (error) {
+                console.error(error);
+            }
+        }
+    };
+
+
     return (
         <>
             <div class="order_torso">
@@ -27,6 +76,7 @@ function Orders() {
                             <th>Table Number</th>
                             <th>Confirmed</th>
                             <th>Order Ready</th>
+                            <th>Cancel Order</th>
                         </tr>
                     </thead>
                     <tbody>
@@ -37,11 +87,18 @@ function Orders() {
                                 <td>{order.tableNumber}</td>
                                 <td><input type="checkbox"></input></td>
                                 <td><input type="checkbox"></input></td>
+                                <td>
+                                    <input 
+                                        type="checkbox" 
+                                        checked={selectedRow[order.id] || false}
+                                        onChange={() => handleOrderSelect(order)}
+                                    />
+                                </td>
                             </tr>
                         ))}
                     </tbody>
                 </Table>
-                <button class="order_button" type="submit" >Cancel Order</button>
+                <button class="order_button" type="submit" onClick={handleCancelClick} >Cancel Order</button>
             </div>
         </>
     );
