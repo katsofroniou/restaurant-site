75d285be8124157775f3dbfc0512af09b6932675
Katerina Sofroniou <katerina.sof@btinternet.com>
2023-03-03 13:24:46 +0000

    feat: created list to show items in basket
    contains dummy data right now

--- frontend/src/pages/Basket.jsx
+++ frontend/src/pages/Basket.jsx
@@ -1,9 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
+import dishes from "./Menu.jsx";
+import "../styling/Basket.css";
+function Basket() {
+    const [basketItems, setBasketItems] = useState([["chocolate", 5, 5], ["marshmallow", 6, 0.6]]);
 
-function Basket () {
-    return(
+    return (
         <>
-            <h1 class='header'>Basket</h1>
+            <h1 className="header">Basket</h1>
+            <ul className="basket-list">
+                {basketItems.map((item, index) => (
+                    <li key={index} className="basket-item">
+                        <div className="item-info">
+                            <div className="item-name">{item[0]}</div>
+                            <div className="item-quantity">({item[1]}x)</div>
+                        </div>
+                        <div className="item-price">£{item[2].toFixed(2)}</div>
+
+                    </li>
+                ))}
+            </ul>
         </>
     );
 }
