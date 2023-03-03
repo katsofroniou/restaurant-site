6cb6777ae5c5c62e45c222185b63da4af17d3433
Katerina Sofroniou <katerina.sof@btinternet.com>
2023-03-03 14:03:32 +0000

    feat: added buttons to change quantity in basket

--- frontend/src/pages/Basket.jsx
+++ frontend/src/pages/Basket.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import dishes from "./Menu.jsx";
 import "../styling/Basket.css";
+
 function Basket() {
     const [basketItems, setBasketItems] = useState([["chocolate", 5, 5], ["marshmallow", 6, 0.6]]);
 
@@ -12,6 +13,20 @@ function Basket() {
         setBasketItems(updatedItems);
     };
 
+    const incrementQuantity = (index) => {
+        const updatedItems = [...basketItems];
+        updatedItems[index][1]++;
+        setBasketItems(updatedItems);
+    };
+
+    const decrementQuantity = (index) => {
+        const updatedItems = [...basketItems];
+        if (updatedItems[index][1] > 1) {
+            updatedItems[index][1]--;
+            setBasketItems(updatedItems);
+        }
+    };
+
     return (
         <>
             <h1 className="header">Basket</h1>
@@ -20,7 +35,11 @@ function Basket() {
                     <li key={index} className="basket-item">
                         <div className="item-info">
                             <div className="item-name">{item[0]}</div>
-                            <div className="item-quantity">({item[1]}x)</div>
+                            <div className="item-quantity">
+                                <button className="quantity-btn" onClick={() => decrementQuantity(index)}>-</button>
+                                {item[1]}
+                                <button className="quantity-btn" onClick={() => incrementQuantity(index)}>+</button>
+                            </div>
                         </div>
                         <div className="item-price">£{item[2].toFixed(2)}</div>
                         <button className="remove-btn" onClick={() => removeItem(index)}>
