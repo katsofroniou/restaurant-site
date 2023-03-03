34bc3d09ba71af8d3dd24c141cbeb2dd1ede8e98
Katerina Sofroniou <katerina.sof@btinternet.com>
2023-03-03 14:22:20 +0000

    chore: converted basket from list to table

--- frontend/src/pages/Basket.jsx
+++ frontend/src/pages/Basket.jsx
@@ -3,53 +3,68 @@ import dishes from "./Menu.jsx";
 import "../styling/Basket.css";
 
 function Basket() {
-    const [basketItems, setBasketItems] = useState([["chocolate", 5, 5], ["marshmallow", 6, 0.6]]);
+  const [basketItems, setBasketItems] = useState([
+    ["chocolate", 5, 5],
+    ["marshmallow", 6, 0.6],
+  ]);
 
-    const removeItem = (index) => {
-        const updatedItems = [
-            ...basketItems.slice(0, index),
-            ...basketItems.slice(index + 1),
-        ];
-        setBasketItems(updatedItems);
-    };
+  const removeItem = (index) => {
+    const updatedItems = [
+      ...basketItems.slice(0, index),
+      ...basketItems.slice(index + 1),
+    ];
+    setBasketItems(updatedItems);
+  };
 
-    const incrementQuantity = (index) => {
-        const updatedItems = [...basketItems];
-        updatedItems[index][1]++;
-        setBasketItems(updatedItems);
-    };
+  const decreaseQuantity = (index) => {
+    const updatedItems = [...basketItems];
+    if (updatedItems[index][1] > 1) {
+      updatedItems[index][1]--;
+      setBasketItems(updatedItems);
+    }
+  };
 
-    const decrementQuantity = (index) => {
-        const updatedItems = [...basketItems];
-        if (updatedItems[index][1] > 1) {
-            updatedItems[index][1]--;
-            setBasketItems(updatedItems);
-        }
-    };
+  const increaseQuantity = (index) => {
+    const updatedItems = [...basketItems];
+    updatedItems[index][1]++;
+    setBasketItems(updatedItems);
+  };
 
-    return (
-        <>
-            <h1 className="header">Basket</h1>
-            <ul className="basket-list">
-                {basketItems.map((item, index) => (
-                    <li key={index} className="basket-item">
-                        <div className="item-info">
-                            <div className="item-name">{item[0]}</div>
-                            <div className="item-quantity">
-                                <button className="quantity-btn" onClick={() => decrementQuantity(index)}>-</button>
-                                {item[1]}
-                                <button className="quantity-btn" onClick={() => incrementQuantity(index)}>+</button>
-                            </div>
-                        </div>
-                        <div className="item-price">£{item[2].toFixed(2)}</div>
-                        <button className="remove-btn" onClick={() => removeItem(index)}>
-                            Remove Item
-                        </button>
-                    </li>
-                ))}
-            </ul>
-        </>
-    );
+  return (
+    <>
+      <h1 className="header">Basket</h1>
+      <table className="basket-table">
+        <thead>
+          <tr>
+            <th>Item</th>
+            <th>Quantity</th>
+            <th>Price</th>
+            <th>Action</th>
+          </tr>
+        </thead>
+        <tbody>
+          {basketItems.map((item, index) => (
+            <tr key={index} className="basket-item">
+              <td>{item[0]}</td>
+              <td>
+                <div className="quantity-buttons">
+                  <button className="quantity-btn" onClick={() => decreaseQuantity(index)}>-</button>
+                  <span className="item-quantity">{item[1]}</span>
+                  <button className="quantity-btn" onClick={() => increaseQuantity(index)}>+</button>
+                </div>
+              </td>
+              <td>£{item[2].toFixed(2)}</td>
+              <td>
+                <button className="remove-btn" onClick={() => removeItem(index)}>
+                  Remove Item
+                </button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </>
+  );
 }
 
 export default Basket;
