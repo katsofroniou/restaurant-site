6d922688337a3a55078aafeb60824c079d5fe532
zkac123 <Natalia.Widmann.2021@live.rhul.ac.uk>
2023-03-07 12:30:29 +0000

    feat: added method to update order status

--- frontend/src/pages/UpdateOrders.jsx
+++ frontend/src/pages/UpdateOrders.jsx
@@ -37,6 +37,54 @@ function UpdateOrders() {
             [selectedOrder.OrderComplete]: !prevState[selectedOrder.OrderComplete],
         }));
     };
+
+    const handleUpdateClick = async () => {
+        // Get the access_token from local storage
+        const access_token = localStorage.getItem('access_token');
+
+        // If access_token is null or empty, the user is not authenticated
+        if (!access_token) {
+            console.log('User not authenticated');
+            return;
+        }
+
+        // Update the selected items
+        const updateItems = [];
+
+        Object.keys(selectedItems).forEach((confirmed) => {
+            if (selectedItems[confirmed]) {
+                updateItems.push(confirmed);
+            }
+        });
+
+        Object.keys(selectedItems).forEach((orderReady) => {
+            if (selectedItems[orderReady]) {
+                updateItems.push(orderReady);
+            }
+        });
+        
+        Object.keys(selectedItems).forEach((OrderComplete) => {
+            if (selectedItems[OrderComplete]) {
+                updateItems.push(OrderComplete);
+            }
+        });
+
+        if (updateItems.length > 0) {
+            try {
+                await Promise.all(updateItems.map(orders => {
+                    return axios({
+                        method: 'POST',
+                        url: `http://127.0.0.1:8000/orders/api/${orders}`,
+                        headers: {
+                            'Authorization' : `Bearer ${access_token}`
+                        },
+                    });
+                }));
+            } catch (error) {
+                console.error(error);
+            }
+        }
+    }
 }
 
 export default UpdateOrders;
