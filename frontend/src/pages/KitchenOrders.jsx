0790d2288c18dba929999df587f319e06b0cb0cb
Jonathan Lloyd <Jonathan>
2023-03-07 16:36:16 +0000

    chore: deletion of redundant code

--- frontend/src/pages/KitchenOrders.jsx
+++ frontend/src/pages/KitchenOrders.jsx
@@ -21,56 +21,6 @@ function Orders() {
         getOrder();
     }, [])
 
-    const handleOrderSelect = (selectedOrder) => {
-        setSelectedRow(prevState => ({
-            ...prevState,
-            [selectedOrder.id]: !prevState[selectedOrder.id],
-        }));
-    };
-
-    const handleCancelClick = async () => {
-        // get access token from local storage
-        const access_token = localStorage.getItem('access_token');
-
-        //if access token is null or empty, user is not authenticated
-        if (!access_token) {
-            console.log('User not authenticated');
-            return;
-        }
-
-        // delete selected orders
-        const deleteOrders = [];
-
-        
-        Object.keys(selectedRow).forEach((id) => {
-            if (selectedRow[id]) {
-                deleteOrders.push(id);
-            }
-        });
-
-        if (deleteOrders.length > 0) {
-            try {
-                await Promise.all(deleteOrders.map(order => {
-                    return axios ({
-                        method: 'DELETE',
-                        url: `http://127.0.0.1:8000/orders/api/${order}`,
-                        headers: {
-                            'Authorization': `Bearer ${access_token}`
-                        },
-                    });
-                }));
-                
-                // update state to remove deleted orders
-                const newOrder = orders.filter((d) => !deleteOrders.includes(d.id));
-                setOrders(newOrder);
-
-            } catch (error) {
-                console.error(error);
-            }
-        }
-    };
-
-
     return (
         <>
             <div class="order_torso">
