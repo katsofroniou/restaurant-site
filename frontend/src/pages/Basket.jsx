bc16768ecc14c5c5df82bfa57a83315a3081d2c3
Katerina Sofroniou <katerina.sof@btinternet.com>
2023-03-08 18:45:13 +0000

    feat: items in localstorage now show in basket table

--- frontend/src/pages/Basket.jsx
+++ frontend/src/pages/Basket.jsx
@@ -1,14 +1,22 @@
-import React, { useState } from "react";
-import dishes from "./Menu.jsx";
+import React, { useState, useEffect } from "react";
 import "../styling/Basket.css";
 
 function Basket() {
-  const [basketItems, setBasketItems] = useState([
-    ["chocolate", 5, 1],
-    ["marshmallow", 6, 0.1],
-  ]);
+  const [basketItems, setBasketItems] = useState([]);
 
-  const totalPrice = basketItems.reduce((acc, item) => acc + item[1] * item[2], 0);
+  useEffect(() => {
+    const storedBasket = JSON.parse(localStorage.getItem("basket")) || [];
+    const newBasketItems = storedBasket.map((item) => ({
+      dish: item.name ? item : item.dish,
+      quantity: item.quantity || 1,
+    }));
+    setBasketItems(newBasketItems);
+  }, []);
+
+  const totalPrice = basketItems.reduce(
+    (acc, item) => acc + item.dish.price * item.quantity,
+    0
+  );
 
   const removeItem = (index) => {
     const updatedItems = [
@@ -20,15 +28,15 @@ function Basket() {
 
   const decreaseQuantity = (index) => {
     const updatedItems = [...basketItems];
-    if (updatedItems[index][1] > 1) {
-      updatedItems[index][1]--;
+    if (updatedItems[index].quantity > 1) {
+      updatedItems[index].quantity--;
       setBasketItems(updatedItems);
     }
   };
 
   const increaseQuantity = (index) => {
     const updatedItems = [...basketItems];
-    updatedItems[index][1]++;
+    updatedItems[index].quantity++;
     setBasketItems(updatedItems);
   };
 
@@ -47,15 +55,15 @@ function Basket() {
         <tbody>
           {basketItems.map((item, index) => (
             <tr key={index} className="basket-item">
-              <td>{item[0]}</td>
+              <td>{item.dish.name}</td>
               <td>
                 <div className="quantity-buttons">
                   <button className="quantity-btn" onClick={() => increaseQuantity(index)}>+</button>
-                  <span className="item-quantity">{item[1]}</span>
+                  <span className="item-quantity">{item.quantity}</span>
                   <button className="quantity-btn" onClick={() => decreaseQuantity(index)}>-</button>
                 </div>
               </td>
-              <td className="price-container">£{(item[2] * item[1]).toFixed(2)}</td>
+              <td className="price-container">£{(item.dish.price * item.quantity).toFixed(2)}</td>
               <td className="remove-btn-container">
                 <button className="remove-btn" onClick={() => removeItem(index)}>
                   Remove Item
