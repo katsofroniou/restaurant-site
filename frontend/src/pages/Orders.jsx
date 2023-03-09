7fcca955b4fd0e5e905a1a6757f8270900efe83b
Jubai-Khalil <jubai.khalil@gmail.com>
2023-03-09 12:29:57 +0000

    feat: added the function for makring order as delivered

--- frontend/src/pages/Orders.jsx
+++ frontend/src/pages/Orders.jsx
@@ -3,6 +3,7 @@ import "../styling/Orders.css";
 import axios from 'axios';
 import { Table } from 'react-bootstrap';
 import { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
 
 
 // This page will only be visible to waiter and kitchen staff - not to the customer
@@ -10,12 +11,62 @@ import { useState, useEffect } from 'react';
 function Orders() {
     const [orders, setOrders] = useState([]);
     const [selectedRow, setSelectedRow] = useState(-1);
+    const [selectedItems, setSelectedItems] = useState({});
 
     useEffect(() => {
         axios.get('http://127.0.0.1:8000/orders/api')
             .then(response => setOrders(response.data))
             .catch(error => console.log(error));
     }, []);
+
+    const handleItemSelect = (selectedDish) => {
+        setSelectedItems(prevState => ({
+            ...prevState,
+            [selectedDish.name]: !prevState[selectedDish.name],
+        }));
+    };
+
+    const handleDeliveredClick = async () => {
+        // Get the access_token from local storage
+        const access_token = localStorage.getItem('access_token');
+
+        // If access_token is null or empty, the user is not authenticated
+        if (!access_token) {
+            console.log('User not authenticated');
+            return;
+        }
+
+        // Delivered the selected items
+        const deliveredItems = [];
+
+        Object.keys(selectedItems).forEach((name) => {
+            if (selectedItems[name]) {
+                deliveredItems.push(name);
+            }
+        });
+
+        if (deliveredItems.length > 0) {
+            try {
+                await Promise.all(deliveredItems.map(dish => {
+                    return axios({
+                        method: 'PATCH', //may change this to delete
+                        url: `http://127.0.0.1:8000/menu/api/${dish}`,
+                        headers: {
+                            'Authorization': `Bearer ${access_token}`
+                        },
+                    });
+                }));
+
+                // Update the state to remove the deleted items
+                const newDish = orders.filter((d) => !deliveredItems.includes(d.name));
+                setOrders(newDish);
+
+            } catch (error) {
+                console.error(error);
+            }
+        }
+    };
+
     return (
         <>
             <div class="order_torso">
@@ -27,6 +78,7 @@ function Orders() {
                             <th>Table Number</th>
                             <th>Confirmed</th>
                             <th>Order Ready</th>
+                            <th>Delivered</th>
                         </tr>
                     </thead>
                     <tbody>
@@ -37,11 +89,15 @@ function Orders() {
                                 <td>{order.tableNumber}</td>
                                 <td><input type="checkbox"></input></td>
                                 <td><input type="checkbox"></input></td>
+                                <td><input type="checkbox"></input></td>
                             </tr>
                         ))}
                     </tbody>
                 </Table>
                 <button class="order_button" type="submit" >Cancel Order</button>
+                <button className="waiter_add_button" onClick={handleDeliveredClick}>
+                <Link to='/orders' class="waiter_button_link">Order Delivered</Link>
+            </button>
             </div>
         </>
     );
