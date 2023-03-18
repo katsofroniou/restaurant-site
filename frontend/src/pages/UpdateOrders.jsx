02039ebff2bba15910beffc8d39ddb32b4747485
Katerina Sofroniou <katerina.sof@btinternet.com>
2023-03-18 13:07:02 +0000

    feat: added ability to update order through tick boxes

--- frontend/src/pages/UpdateOrders.jsx
+++ frontend/src/pages/UpdateOrders.jsx
@@ -7,152 +7,64 @@ import axios from 'axios';
 
 function UpdateOrders() {
     const [orders, setOrders] = useState([]);
-    const [selectedConfirmedItems, setSelectedConfirmedItems] = useState([]);
-    const [selectedReadyItems, setSelectedReadyItems] = useState([]);
-    const [selectedCompleteItems, setSelectedCompleteItems] = useState({});
-    const [selectedRow, setSelectedRow] = useState({});
 
-    const getOrder = async () => {
+    const access_token = localStorage.getItem('access_token');
+
+    const config = {
+        headers: { Authorization: `Bearer ${access_token}` }
+    };
+
+    const getOrders = async () => {
         const response = await axios.get('http://127.0.0.1:8000/orders/api')
         setOrders(response.data)
     }
 
     useEffect(() => {
-        getOrder();
+        getOrders();
     }, [])
 
-    const handleConfirmSelect = (selectedOrder) => {
-        setSelectedConfirmedItems(
-            selectedConfirmedItems => [...selectedConfirmedItems, selectedOrder]
-        );
-    };
-    
-    const handleReadySelect = (selectedOrder) => {
-        //console.log(selectedOrder); //DEBUGS
-        setSelectedReadyItems(
-            selectedReadyItems => [...selectedReadyItems, selectedOrder]
-        );
+    const handleConfirmSelect = (orderToUpdate) => {
+        const updatedOrders = orders.map(order => {
+            if (order.id === orderToUpdate.id) {
+                order.confirmed = !order.confirmed;
+            }
+            return order;
+        });
     };
 
-    const handleCompleteSelect = (selectedOrder) => {
-        setSelectedCompleteItems(prevState => ({
-            ...prevState,
-            [selectedOrder.OrderComplete]: !prevState[selectedOrder.OrderComplete],
-        }));
+    const handleReadySelect = (orderToUpdate) => {
+        const updatedOrders = orders.map(order => {
+            if (order.id === orderToUpdate.id) {
+                order.orderReady = !order.orderReady;
+            }
+            return order;
+        });
+    };
+
+    const handleCompleteSelect = (orderToUpdate) => {
+        const updatedOrders = orders.map(order => {
+            if (order.id === orderToUpdate.id) {
+                order.OrderComplete = !order.OrderComplete;
+            }
+            return order;
+        });
     };
 
     const handleUpdateClick = async () => {
-        // Get the access_token from local storage
-        const access_token = localStorage.getItem('access_token');
-
-        // If access_token is null or empty, the user is not authenticated
-        if (!access_token) {
-            console.log('User not authenticated');
-            return;
-        }
-
-        // Update the selected items
-        const updateConfirmed = [];
-        const updateReady = [];
-        const updateComplete = [];
-
-        // Object.keys(selectedConfirmedItems).forEach((confirmed) => {
-        //     if (selectedConfirmedItems[confirmed]) {
-        //       //console.log(selectedConfirmedItems); //DEBUG
-        //         updateConfirmed.push(confirmed);
-        //     }
-        // });
-
-        for( let i = 0; i < selectedConfirmedItems; i++ ) {
-           let confirmObj = selectedConfirmedItems[i]
-           confirmObj.confirmed = !confirmObj.confirmed;
-           updateConfirmed.push(confirmObj)
-        }
-        // Object.keys(selectedReadyItems).forEach((orderReady) => {
-        //     if (selectedReadyItems[orderReady]) {
-        //         updateReady.push(orderReady);
-        //     }
-            
-        // });
-
-        for( let i = 0; i < selectedReadyItems.length; i++ ) {
-           let currObj = selectedReadyItems[i]
-           currObj.orderReady = !currObj.orderReady;
-           updateReady.push(currObj)
-        }
-        
-        // Object.keys(selectedCompleteItems).forEach((OrderComplete) => {
-        //    if (selectedCompleteItems[OrderComplete]) {
-        //        updateComplete.push(OrderComplete);
-        //    }
-        // });
-
-        for( let i = 0; i < selectedCompleteItems.length; i++ ) {
-            let compObj = selectedCompleteItems[i]
-            compObj.OrderComplete = !compObj.OrderComplete;
-            updateComplete.push(compObj)
-        }
-
-        console.log("sending...", updateConfirmed); //DEBUG
-        if (updateConfirmed.length > 0) {
-            try {
-                await Promise.all(updateConfirmed.map(order => {
-                    order = 10;
-                    return axios({
-                        method: 'PUT',
-                        url: `http://127.0.0.1:8000/orders/api/${order}/`,
-                        headers: {
-                            'Authorization' : `Bearer ${access_token}`
-                        },
-                        data: {
-                          confirmed: true,
-                        }}); 
-                }));
-            } catch (error) {
-                console.error(error);
+        try {
+            for (let i = 0; i < orders.length; i++) {
+                const response = await axios.put(
+                    `http://127.0.0.1:8000/orders/api/${orders[i].id}/`,
+                    orders[i],
+                    config
+                );
+                console.log(response);
             }
-        }
 
-        console.log("sending...", updateReady); //DEBUG
-        if (updateReady.length > 0) {
-            try {
-                await Promise.all(updateReady.map(order => {
-                    order = 10;
-                    return axios({
-                        method: 'PUT',
-                        url: `http://127.0.0.1:8000/orders/api/${order}/`,
-                        headers: {
-                            'Authorization' : `Bearer ${access_token}`
-                        },
-                        data: {
-                          orderReady: true,
-                        }});
-                }));
-            } catch (error) {
-                console.error(error);
-            }
+            setOrders(orders)
+        } catch (error) {
+            console.log(error);
         }
-
-        console.log("sending...", updateComplete); //DEBUG
-        if (updateComplete.length > 0) {
-            try {
-                await Promise.all(updateComplete.map(order => {
-                    order = 10;
-                    return axios({
-                        method: 'PUT',
-                        url: `http://127.0.0.1:8000/orders/api/${order}/`,
-                        headers: {
-                            'Authorization' : `Bearer ${access_token}`
-                        },
-                        data: {
-                          OrderComplete: true,
-                        }});
-                }));
-            } catch (error) {
-                console.error(error);
-            }
-        }
-        //window.location = "/updateorders";
     };
 
     return (
@@ -179,28 +91,25 @@ function UpdateOrders() {
                                 {order.confirmed === true && <td class="updateOrder_td">Confirmed</td>}
                                 {order.confirmed === false && <td class="updateOrder_td">Unconfirmed</td>}
                                 <td class="updateOrder_td">
-                                    <input 
-                                        type = "checkbox"
-                                        //checked = {selectedConfirmedItems[order.confirmed] || false}
-                                        onChange = {() => handleConfirmSelect(order)}
+                                    <input
+                                        type="checkbox"
+                                        onChange={() => handleConfirmSelect(order)}
                                     />
                                 </td>
                                 {order.orderReady === true && <td class="updateOrder_td">Ready</td>}
                                 {order.orderReady === false && <td class="updateOrder_td">Not Ready</td>}
                                 <td class="updateOrder_td">
-                                    <input 
-                                        type = "checkbox"
-                                        //checked = {order.orderReady}
-                                        onChange = {() => handleReadySelect(order)}
+                                    <input
+                                        type="checkbox"
+                                        onChange={() => handleReadySelect(order)}
                                     />
                                 </td>
                                 {order.OrderComplete === true && <td class="updateOrder_td">Complete</td>}
                                 {order.OrderComplete === false && <td class="updateOrder_td">Not Complete</td>}
                                 <td class="updateOrder_td">
                                     <input
-                                        type = "checkbox"
-                                        //checked = {selectedCompleteItems[order.OrderComplete] || false}
-                                        onChange = {() => handleCompleteSelect(order)} 
+                                        type="checkbox"
+                                        onChange={() => handleCompleteSelect(order)}
                                     />
                                 </td>
                             </tr>
