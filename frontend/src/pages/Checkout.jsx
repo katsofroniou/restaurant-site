8a2d660e611bdde3cb3b15ed7f55f2bc770ccf40
Katerina Sofroniou <katerina.sof@btinternet.com>
2023-03-20 21:37:40 +0000

    chore: renamed css file for Checkout page

--- frontend/src/pages/Checkout.jsx
+++ frontend/src/pages/Checkout.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 import { Elements } from '@stripe/react-stripe-js';
 import { loadStripe } from '@stripe/stripe-js';
 import CheckoutForm from './CheckoutForm';
-import '../styling/StripeApp.css';
+import '../styling/Checkout.css';
 
 const stripePromise = loadStripe('pk_test_51Mm1OUGHCVd3YY0Z0qddQGTs0mndGS26E7ooPejSxkUGOxgPQs0BBF6pz5V5Oxf9gcJlP4vphcyhpPxqAtqQqkyd00QO4WujFi');
 
