7bf9f7b9d18bfd06e68a9892f495b10fb34ea63c
Jubai-Khalil <jubai.khalil@gmail.com>
2023-03-21 12:16:37 +0000

    feat: function for customers to track order status

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,14 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import "../styling/Home.css";
 import image from '../Images/homepage_image2.png'
 import image2 from '../Images/seating.jpeg'
 import image3 from '../Images/food.jpeg'
 import image4 from '../Images/table.jpeg'
-import "../styling/Home.css";
+import axios from 'axios';
 
 
 function Home () {
-    return(
+  const [orderId, setOrderId] = useState('');
+  const [orderInfo, setOrderInfo] = useState(null);
+
+  const handleInputChange = (e) => {
+      setOrderId(e.target.value);
+  };
+
+  const handleSubmit = async (e) => {
+      e.preventDefault();
+
+      try {
+          const response = await axios.get(`http://127.0.0.1:8000/orders/api/${orderId}`);
+          setOrderInfo(response.data);
+      } catch (error) {
+          console.error(error);
+          setOrderInfo(null);
+      }
+  };
+
+  return (
         <>
             <img background-size= "background" src={image}></img> 
             <div className="infoBoxContainer">
@@ -36,8 +55,30 @@ function Home () {
                 <button className="button3" type="submit">MORE</button>
               </div>
             </div>
+            <div className="orderInput">
+                    <form onSubmit={handleSubmit}>
+                        <input
+                            type="text"
+                            value={orderId}
+                            onChange={handleInputChange}
+                            placeholder="Enter Your Order ID"
+                            className="orderInputField"
+                        />
+                        <button type="submit" className="button4">Check Order Status</button>
+                    </form>
+                </div>
+                {orderInfo && (
+                  <div className="infoBoxContainer2">
+                    <div className="orderStatus">
+                        <p>Confirmed: {orderInfo.confirmed ? "Yes" : "No"}</p>
+                        <p>Order Ready: {orderInfo.orderReady ? "Yes" : "No"}</p>
+                        <p>Order Delivered: {orderInfo.OrderDelivered ? "Yes" : "No"}</p>
+                        <p>Order Complete: {orderInfo.OrderComplete ? "Yes" : "No"}</p>
+                    </div>
+                    </div>
+                )}
         </>
-    );
+  );
 }
 
 export default Home;
