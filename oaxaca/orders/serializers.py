fe0745ed299cf8e633e2562c809393243aa3b34f
Katerina Sofroniou <katerina.sof@btinternet.com>
2023-03-21 05:31:16 +0000

    feat: created serializer for OrderDish

--- oaxaca/orders/serializers.py
+++ oaxaca/orders/serializers.py
@@ -1,7 +1,12 @@
 from rest_framework import serializers
-from .models import Order
+from .models import Order, OrderDish
 
 class OrderSerializer(serializers.ModelSerializer):
     class Meta:
         model = Order
         fields = ["id", "orderTime", "tableNumber", "items", "confirmed", "orderReady", "OrderComplete"]
+        
+class OrderDishSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = OrderDish
+        fields = ['id', 'order', 'dish', 'quantity']
