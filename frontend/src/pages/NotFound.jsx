03168d64b8619ae97670532195994a585c66ef7a
zkac123 <Natalia.Widmann.2021@live.rhul.ac.uk>
2023-03-21 23:45:29 +0000

    Feat: added NotFound page documentation

--- frontend/src/pages/NotFound.jsx
+++ frontend/src/pages/NotFound.jsx
@@ -1,5 +1,14 @@
 import React from "react";
+/**
+ * @author Natalia Widmann
+ * @file NotFound.jsx contains the PageNotFound page.
+ * @description This page is routed to when the user attempts to go to an unavailable page.
+ */
 
+/**
+ * @function NotFound
+ * @returns {void} Returns the constructed PageNotFound page.
+ */
 function NotFound() {
     return(
         <h1>Error 404: Page not Found</h1>
