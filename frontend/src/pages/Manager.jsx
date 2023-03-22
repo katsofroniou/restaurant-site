d8eb052a557b4912499256da7c845a304c031cbd
zkac123 <Natalia.Widmann.2021@live.rhul.ac.uk>
2023-03-22 02:15:04 +0000

    feat: added documentation for the manager file

--- frontend/src/pages/Manager.jsx
+++ frontend/src/pages/Manager.jsx
@@ -2,21 +2,55 @@ import React, { useState, useEffect } from "react";
 import { Link } from 'react-router-dom';
 import "../styling/Manager.css";
 import axios from 'axios';
+/**
+ * @author Natalia Widmann
+ * @file Manager.jsx contains the frontend page with the list of users for the manager.
+ */
 
+/**
+ * @function Manager
+ * @returns {JSX.Element} Returns the constructed page for the Manager.
+ */
 function Manager () {
 
+    /**
+     * State hook to manage a list of users and a selected user object.
+     * @typedef {Object} UserState
+     * @property {Object[]} user - The list of user objects.
+     * @property {function} setUser - The setter function to update the list of users.
+     * @property {Object} selectedUser - The selected user object.
+     * @property {function} setSelectedUser - The setter function to update the selected user object.
+     */
     const [user, SetUser] = useState([]);
     const [selectedUser, setSelectedUser] = useState({});
 
+    /**
+     * Retrieves a list of users from the server and sets them as the state of the component.
+     * @function getUser
+     * @async
+     * @returns {Promise<void>}
+     */
     const getUser = async () => {
         const response = await axios.get('http://127.0.0.1:8000/users/api')
         SetUser(response.data)
     }
 
+    /**
+     * A hook that is called on mount to retrieve the user data from the server and set it as the state of the component.
+     * @function
+     * @returns {void}
+     */
     useEffect(() => {
         getUser();
     }, [])
 
+    /**
+     * Selects an user and updates the selected user state.
+     * @function handleUserSelect
+     * @param {Object} selectedUser - The selected order object.
+     * @param {Object} selectedUser.name - The name of the selected user.
+     * @returns {void}
+     */
     const handleUserSelect = (selectedUser) => {
         setSelectedUser(prevState => ({
             ...prevState,
@@ -24,6 +58,12 @@ function Manager () {
         }));
     };
 
+    /**
+     * Handles the delete user button click event
+     * @async
+     * @function
+     * @returns {Promise<void>}
+     */
     const handleDeleteUserClick = async () => {
         // Get the access_token from local storage
         const access_token = localStorage.getUser('access_token');
@@ -64,6 +104,7 @@ function Manager () {
             }
         }
     }
+
     return(
         <>
             <table class="user_menu_table">
