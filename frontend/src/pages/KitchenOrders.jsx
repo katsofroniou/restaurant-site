01c6a5781bf29d2b34dc8694e864a41043d10d23
zkac123 <Natalia.Widmann.2021@live.rhul.ac.uk>
2023-03-22 03:17:45 +0000

    feat: added documentation for KitchenOrders

--- frontend/src/pages/KitchenOrders.jsx
+++ frontend/src/pages/KitchenOrders.jsx
@@ -3,18 +3,36 @@ import { Table } from 'react-bootstrap';
 import { useState, useEffect } from 'react';
 import "../styling/KitchenOrders.css";
 import axios from 'axios';
+/**
+ * @author Kayleigh Reid
+ * @author Jonathan Lloyd
+ * @file KitchenOrders.jsx contains the frontend page with the Kitchen Staff display of orders.
+ */
 
-
-// This page will only be visible to waiter and kitchen staff - not to the customer
-
+/**
+ * @function KitchenOrders
+ * @returns {JSX.Element} Returns the constructed page for KitchenOrders.
+ */
 function KitchenOrders() {
+    /**
+     * A stateful value of an order
+     * @typedef {any[]} OrdersState - Array of objects representing the orders.
+     * @type {OrdersState}
+     */
     const [orders, setOrders] = useState([]);
 
+    /**
+     * Fetches orders from the server and sets orders state with the response data.
+     * @async
+     * @function getOrder
+     * @returns {Promise<void>}
+     */
     const getOrder = async () => {
         const response = await axios.get('http://127.0.0.1:8000/orders/api')
         setOrders(response.data)
     }
-
+    
+    // Run getOrder on component mount
     useEffect(() => {
         getOrder();
     }, [])
