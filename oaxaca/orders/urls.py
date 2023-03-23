9d741d7bdfa1b75cc6affd43207d263bf219a02a
Katerina Sofroniou <katerina.sof@btinternet.com>
2023-03-23 00:32:51 +0000

    fix: removed url conflicts

--- oaxaca/orders/urls.py
+++ oaxaca/orders/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import OrderApiView, OrderDetailApiView
+from .views import OrderApiView, OrderDetailApiView, OrderDishApiView
 
 urlpatterns = [
     path('api', OrderApiView.as_view()),
-    path('api/<OrderVal>/', OrderDetailApiView.as_view())
+    path('api/<OrderVal>/', OrderDetailApiView.as_view()),
+    path('orderDish/', OrderDishApiView.as_view())
 ]
