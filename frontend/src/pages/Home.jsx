a8d56467c7eb3eb29f80836a5899b5ba886c2d59
Katerina Sofroniou <katerina.sof@btinternet.com>
2023-03-23 09:55:12 +0000

    chore: fixed card spacing

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -24,19 +24,16 @@ function Home () {
                 <img style={{ width: 300, height: 250,}} src={image2}></img> 
                 <h3 className="title">WELCOME</h3>
                 <p className="description">Join us for lunch or dinner at Oaxaca, where the vibrancy of the Mexican market meets the contemporary coolness of a modern-day restaurant in an explosion of colors and textures. In addition, we pride ourselves on the integrity of our flavors and the sourcing of our products.</p>
-                <button className="button" type="submit">MORE</button>
               </div>
               <div className="infoBox">
                 <img style={{ width: 300, height: 250 }} src={image3}></img> 
                 <h3 className="title2">ABOUT</h3>
                 <p className="description">Our constantly evolving seasonal menu at Oaxaca is a result of our efforts to harmonize Mexican flavors with locally sourced ingredients that are grown and transported with environmental consciousness. We strive to procure ingredients that are readily available, ensuring that our menu is ever-changing.</p>
-                <button className="button2" type="submit">MORE</button>
               </div>
               <div className="infoBox">
                 <img style={{ width: 300, height: 250 }} src={image4}></img> 
                 <h3 className="title3">HISTORY</h3>
                 <p className="description">Gone are the days when cheap tequila shots, overpowering spices, and greasy tortilla chips were the only options that came to mind when craving Mexican food. At Oaxaca, we have revolutionized the dining experience by doing things a little differently.</p>
-                <button className="button3" type="submit">MORE</button>
               </div>
             </div>
         </>
