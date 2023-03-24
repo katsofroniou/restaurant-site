a798d0b3e7d99f1cf96483d26225ad34a92f9b68
Katerina Sofroniou <katerina.sof@btinternet.com>
2023-03-24 05:41:54 +0000

    feat: created api view to get/delete individual users

--- oaxaca/accounts/views.py
+++ oaxaca/accounts/views.py
@@ -7,7 +7,7 @@ from django.contrib.auth.models import User
 class UserCreate(APIView):
     # Allows everyone to use safe api options - GET, HEAD, OPTIONS
     # Allows authenticated users to use unsafe api options - POST, DELETE, PUT
-    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    permission_classes = [permissions.IsAuthenticated]
     
     # Post api to create new user
     def post(self, request, format='json'):
@@ -26,3 +26,39 @@ class UserCreate(APIView):
         serializer = UserSerializer(user, many=True)
         
         return Response(serializer.data, status=status.HTTP_200_OK)
+     
+class UserDetailApiView(APIView):
+    permission_classes = [permissions.IsAdminUser]
+    
+    def get_object(self, userId, *args, **kwargs):
+        try:
+            return User.objects.get(id=userId)
+        except:
+            return None
+        
+    def get(self, request, userId, *args, **kwargs):
+        user = self.get_object(userId)
+        
+        if not user:
+            return Response (
+                {"res": "User with this id does not exist"},
+                status = status.HTTP_400_BAD_REQUEST
+            )
+            
+        serializer = UserSerializer(user)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+         
+    def delete(self, request, userId, *args, **kwargs):
+        user_instance = self.get_object(userId)
+        
+        if not user_instance:
+            return Response(
+                {"res": "User with this id does not exist"},
+                status=status.HTTP_400_BAD_REQUEST   
+            )
+        
+        user_instance.delete()
+        return Response(
+            {"res": "User deleted!"},
+            status=status.HTTP_200_OK
+        )
