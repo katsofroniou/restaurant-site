96645beac7d1fd8c6242229e2b7e269ff2638bbf
Katerina Sofroniou <katerina.sof@btinternet.com>
2023-03-24 05:42:46 +0000

    feat: created url for detailed user api

--- oaxaca/accounts/urls.py
+++ oaxaca/accounts/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import UserCreate
+from .views import UserCreate, UserDetailApiView
 
 urlpatterns = [
     # Adds user create as an api view
     path('api', UserCreate.as_view(), name='account-create'),
+    path('api/<userId>', UserDetailApiView.as_view(), name='account')
 ]
